1e4739c82944f5f1435e497b96e92fc1d51be97e
pagreene <patrick.anton.greene@gmail.com>
2018-09-28 16:59:59 -0400

    Add m2r dependency to allow incorporation of md files.

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
-    'citations'
+    'citations',
+    'm2r'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +50,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'

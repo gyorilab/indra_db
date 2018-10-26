0014389ee09857cd048352756295551dbf9afd70
John Bachman <bachmanjohn@gmail.com>
2018-10-26 15:15:58 -0400

    Add evidence_ids to PMID links

--- rest_api/html_format.py
+++ rest_api/html_format.py
@@ -4,12 +4,14 @@ Format a set of INDRA Statements into an HTML-based format.
 import sys
 import csv
 from os.path import abspath, dirname, join
+import re
 import json
 from collections import defaultdict
 from jinja2 import Template
 from indra.sources import indra_db_rest
 from indra.assemblers.english import EnglishAssembler
 from indra.statements import stmts_from_json
+from indra.databases import get_identifiers_url
 
 # Create a template object from the template file, load once
 template_path = join(dirname(abspath(__file__)), 'template.html')
@@ -17,17 +19,43 @@ with open(template_path, 'rt') as f:
     template_str = f.read()
     template = Template(template_str)
 
+# TODO:
+# - Highlight text in english assembled sentences
+# - Highlight text in evidence sentences
+# - For both, add links to identifiers.org
 
-def format_evidence_text(stmt):
+
+
+def format_evidence_text(stmt, stmt_hash):
     """Highlight subject and object in raw text strings."""
     ev_list = []
-    for ev in stmt.evidence:
+    for ix, ev in enumerate(stmt.evidence):
         if ev.text is None:
-            formatted_text = '(None available)'
-
-        formatted_text = ev.text
+            format_text = '(None available)'
+        else:
+            format_text = ev.text
         """
-        ag_name_list = [ag.name for ag in agent_list]
+            indices = {}
+            for ix, ag in enumerate(stmt.agent_list()):
+                ag_text = ev.annotations['agents']['raw_text'][ix]
+                if ag_text is None:
+                    continue
+                marker = '<span class="label label-primary">'
+                indices[ag_text] = [(m.start(), m.start() + len(ag_text))
+                                    for m in re.finditer(ag_text, ev.text)]
+                indices 
+                n = len(ag_text)
+                last = 0
+                for s in starts:
+                    in
+                    ag_start_ix = format_text.find(ag_text)
+                    ag_end_ix = ag_start_ix + len(ag_text)
+                    format_text = (format_text[0:ag_start_ix] + marker +
+                                  format_text[ag_start_ix:ag_end_ix] + '</span>' +
+                                   format_text[ag_end_ix:])
+        """
+        """
+        ag_name_list = [(ix, ag.name) for ix, ag in enumerate(agent_list)]
         #subj_ag_index = ag_name_list.index(edge[0])
         #obj_ag_index = ag_name_list.index(edge[1])
         #subj_text = evidence.annotations['agents']['raw_text'][subj_ag_index]
@@ -48,8 +76,10 @@ def format_evidence_text(stmt):
                            format_text[ag_end_ix:])
         return format_text
         """
+        evidence_id = f"{stmt_hash}-{ix}"
         ev_list.append({'source_api': ev.source_api,
-                        'pmid': ev.pmid, 'text': formatted_text})
+                        'pmid': ev.pmid, 'text': format_text,
+                        'evidence_id': evidence_id})
     return ev_list
 
 
@@ -79,7 +109,7 @@ def format_statements(result):
         stmt = stmts_from_json([stmt_json])[0] # TODO: Need to go back to stmt?
         ea = EnglishAssembler([stmt])
         english = ea.make_model()
-        ev_list = format_evidence_text(stmt)
+        ev_list = format_evidence_text(stmt, stmt_hash)
         total_evidence = result['evidence_totals'][int(stmt_hash)]
         stmts_formatted.append({
             'hash': stmt_hash,

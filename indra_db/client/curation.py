61b011e2d91935858bf8ce90b023f10f7183c0df
pagreene <patrick.anton.greene@gmail.com>
2018-11-09 16:11:41 -0500

    Update curation api.

--- indra_db/client/curation.py
+++ indra_db/client/curation.py
@@ -6,8 +6,8 @@ from indra_db.util import get_primary_db
 from indra_db.exceptions import NoAuthError
 
 
-def submit_curation(level, hash_val, tag, text, curator, ip, api_key,
-                    source='direct_client', db=None):
+def submit_curation(hash_val, tag, curator, ip, api_key, text=None,
+                    ev_hash=None, source='direct_client', db=None):
     """Submit a curation for a given preassembled or raw extraction.
 
     Parameters
@@ -19,69 +19,54 @@ def submit_curation(level, hash_val, tag, text, curator, ip, api_key,
         The hash corresponding to the statement.
     tag : str
         A very short phrase categorizing the error or type of curation.
-    text : str
-        A brief description of the problem.
     curator : str
         The name or identifier for the curator.
     ip : str
         The ip address of user's computer.
+    api_key : str
+        If you have one, this can help identify you as a curator, and may lend
+        extra weight to your curation(s).
+    text : str
+        A brief description of the problem.
+    ev_hash : int
+        A hash of the sentence and other evidence information. Elsewhere
+        referred to as `source_hash`.
     source : str
         The name of the access point through which the curation was performed.
         The default is 'direct_client', meaning this function was used
         directly. Any higher-level application should identify itself here.
-    api_key : str
-        If you have one, this can help identify you as a curator, and may lend
-        extra weight to your curation(s).
     db : DatabaseManager
         A database manager object used to access the database.
     """
     if db is None:
         db = get_primary_db()
 
-    if level not in ['pa', 'raw']:
-        raise ValueError('Level must be either "pa" or "raw", but got "%s".'
-                         % level)
-
     inp = {'tag': tag, 'text': text, 'curator': curator, 'ip': ip,
-           'source': source}
+           'source': source, 'pa_hash': hash_val, 'source_hash': ev_hash}
 
     auth = db._get_auth_info(api_key)
     if auth is None:
         raise NoAuthError(api_key, 'curation')
     inp['auth_id'] = auth[0]
 
-    if level == 'pa':
-        cur = db.PACuration
-        inp['pa_hash'] = hash_val
-    else:
-        cur = db.RawCuration
-        inp['source_hash'] = hash_val
-
     logger.info("Adding curation: %s" % str(inp))
 
-    dbid = db.insert(cur, **inp)
-    return level, dbid
+    dbid = db.insert(db.Curation, **inp)
+    return dbid
 
 
-def get_curations(level, db=None, **params):
+def get_curations(db=None, **params):
     """Get all curations for a certain level given certain criteria."""
     if db is None:
         db = get_primary_db()
-
-    if level not in ['pa', 'raw']:
-        raise ValueError('Level must be either "pa" or "raw", but got "%s".'
-                         % level)
-    if level == 'pa':
-        cur = db.PACuration
-        hash_key = 'pa_hash'
-    else:
-        cur = db.RawCuration
-        hash_key = 'source_hash'
+    cur = db.Curation
 
     constraints = []
     for key, val in params.items():
         if key == 'hash_val':
-            key = hash_key
+            key = 'pa_hash'
+        if key == 'ev_hash':
+            key = 'source_hash'
         if isinstance(val, list) or isinstance(val, set) \
            or isinstance(val, tuple):
             constraints.append(getattr(cur, key).in_(val))

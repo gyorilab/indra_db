d082da0e009956a538d56b760afa8d7a4e62e6ae
Ben Gyori <ben.gyori@gmail.com>
2019-02-11 23:51:02 -0500

    Add basic setup and see if Travis works

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup, find_packages
+
+def main():
+    setup(name='indra_db',
+          version='0.0.1',
+          description='INDRA Database',
+          long_description='INDRA Database',
+          url='https://github.com/indralab/indra_db',
+          packages=find_packages()
+          install_requires=['boto3', 'sqlalchemy', 'psycopg2-binary',
+                            'pgcopy', 'matplotlib', 'flask', 'nltk',
+                            'reportlab']
+          )

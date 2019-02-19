96e12cb48d70423a47c557b04ae8b3b2fc518a07
pagreene <patrick.anton.greene@gmail.com>
2019-02-19 17:00:19 -0500

    Create code to create the lambda.

--- rest_api/secure.py
+++ rest_api/secure.py
@@ -36,10 +36,62 @@ class SecurityManager(object):
 
     def __init__(self, stage):
         with open(join(HERE, pardir, 'zappa_settings.json'), 'r') as f:
-            info = json.load(f)
-        deployment_info = info[stage]
-        self.function_name = deployment_info['project_name'] + '-' + stage
+            zappa_info = json.load(f)
+        self.info = zappa_info[stage]
+        self.function_name = self.info['project_name'] + '-' + stage
+        self._zip_files = []
         return
+
+    def get_zappa_role(self):
+        return self.function_name + '-ZappaLambdaExecutionRole'
+
+    def package_lambdas(self):
+        """Create a zip file for the lambdas."""
+        # Package up the env
+        zip_path = shutil.make_archive(join(HERE, 'lambda'), 'zip', sys.prefix)
+        self._zip_files.append(zip_path)
+
+        # Add the relevant files from indra_db.
+        idbr_dir = join(HERE, pardir, 'indra_db')
+        with ZipFile(zip_path, 'a') as zf:
+            zf.write(join(idbr_dir, 'managers', 'database_manager.py'),
+                     'indra_db/database_manager.py')
+            zf.write(join(idbr_dir, 'util', '__init__.py'),
+                     'indra_db/util/__init__.py')
+            zf.write(join(idbr_dir, '__init__.py'),
+                     'indra_db/__init__.py')
+            zf.write(join(idbr_dir, 'exceptions.py'),
+                     'indra_db/exceptions.py')
+            zf.write(join(HERE, 'security_lambdas', 'verify_key_script.py'),
+                     'verify_key_script.py')
+        return zip_path
+
+    def _clear_packages(self):
+        """Remove any zip files that were created."""
+        for zip_path in self._zip_files:
+            if exists(zip_path):
+                remove(zip_path)
+        return
+
+    def create_lambdas(self):
+        """Create the necessary lambda functions."""
+        try:
+            # Package the environment and code.
+            zip_path = self.package_lambdas()
+
+            lamb = boto3.client('lambda')
+            with open(zip_path, 'rb') as zf:
+                fname = self.function_name + '-auth'
+                env = {'Variables': self.info['environment_variables']}
+                lamb.create_function(
+                    FunctionName=fname, Runtime=self.info['runtime'],
+                    Role=self.get_zappa_role(), Code={'ZipFile': zf.read()},
+                    VpcConfig=self.info['vpc_config'], Environment=env,
+                    Handler='verify_key_script.lambda_handler',
+                    Tags={'project': 'cwc'}
+                    )
+        finally:
+            self._clear_packages()
 
     def update_lambdas(self):
         """Update the verification and api key creation lambdas.
@@ -48,42 +100,26 @@ class SecurityManager(object):
         packaged. The env should be minimal because lambdas have a pretty strict
         size limit.
         """
-        zip_path = None
         try:
-            # Package up the env
-            zip_path = shutil.make_archive(join(HERE, 'lambda'), 'zip', sys.prefix)
-
-            # Add the relevant files from indra_db.
-            idbr_dir = join(HERE, pardir, 'indra_db')
-            with ZipFile(zip_path, 'a') as zf:
-                zf.write(join(idbr_dir, 'managers', 'database_manager.py'),
-                         'indra_db/database_manager.py')
-                zf.write(join(idbr_dir, 'util', '__init__.py'),
-                         'indra_db/util/__init__.py')
-                zf.write(join(idbr_dir, '__init__.py'),
-                         'indra_db/__init__.py')
-                zf.write(join(idbr_dir, 'exceptions.py'),
-                         'indra_db/exceptions.py')
-                zf.write(join(HERE, 'security_lambdas', 'verify_key_script.py'),
-                         'verify_key_script.py')
+            # Package the lambda
+            zip_path = self.package_lambdas()
 
             # Update the lambda.
             lamb = boto3.client('lambda')
-            with open(join(HERE, 'lambda.zip'), 'rb') as zf:
+            with open(zip_path, 'rb') as zf:
                 fname = self.function_name + '-auth'
                 ret = lamb.update_function_code(ZipFile=zf.read(),
                                                 FunctionName=fname)
                 print(ret)
         finally:
-            if zip_path is not None and exists(zip_path):
-                remove(zip_path)
+            self._clear_packages()
 
 
 def get_parser():
     parser = ArgumentParser(description='Apply and update the security to '
                                         'the database REST API.')
     parser.add_argument('action',
-                        choices=['update-lambdas'],
+                        choices=['update-lambdas', 'create-lambdas'],
                         help='Select which action to perform.')
     parser.add_argument('stage', help='Select which stage to operate on.')
     return parser
@@ -96,6 +132,9 @@ def main():
 
     if args.action == 'update-lambdas':
         sec_man.update_lambdas()
+    elif args.action == 'create-lambdas':
+        sec_man.create_lambdas()
+
     return
 
 

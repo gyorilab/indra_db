7166b69d31195c7efc3041b6fe7a851d44a40428
pagreene <patrick.anton.greene@gmail.com>
2019-03-11 10:33:01 -0400

    Improve TAS test further.

--- indra_db/tests/test_datasets.py
+++ indra_db/tests/test_datasets.py
@@ -8,7 +8,13 @@ from indra_db.util import get_test_db
 def test_tas():
     db = get_test_db()
     db._clear(force=True)
+    dbid = db.select_one(db.DBInfo.id, db.DBInfo.db_name == TasManager.name)
+    assert dbid is None
     tm = TasManager()
     tm.upload(db)
+    dbid = db.select_on(db.DBInfo, db.DBInfo.db_name == TasManager.name)
+    assert dbid is not None
     db_stmts = db.select_all(db.RawStatements)
+    print(len(db_stmts))
     assert len(db_stmts)
+    assert all(s.db_info_id == dbid for s in db_stmts)

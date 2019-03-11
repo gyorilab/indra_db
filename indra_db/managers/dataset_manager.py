b8b859008d96a7a9aebf1d8971fcdebc63d84766
kkaris <karis.klas@gmail.com>
2019-03-11 15:29:39 -0400

    Add CBNManager

--- indra_db/managers/dataset_manager.py
+++ indra_db/managers/dataset_manager.py
@@ -1,6 +1,13 @@
+import os
+import shutil
 import logging
+import urllib.request as urllib_request
 
+from zipfile import ZipFile
+from indra.sources.bel.api import process_jgif_file
 from indra_db.util import insert_db_stmts
+
+CBN_DN = 'http://www.causalbionet.com/Content/jgf_bulk_files/Human-2.0.zip'
 
 logger = logging.getLogger(__name__)
 
@@ -45,3 +52,29 @@ class SignorManager(DatasetManager):
         from indra.sources.signor import process_from_web
         proc = process_from_web()
         return proc.statements
+
+
+class CBNManager(object):
+    """This manager handles retrieval and processing of CBN network files"""
+
+    @staticmethod
+    def _get_statements():
+        tmp_archive = './temp_cbn_human.zip'
+        temp_extract = './temp/'
+        logger.info('Retrieving CBN network zip archive')
+        response = urllib_request.urlretrieve(url=CBN_DN, filename=tmp_archive)
+        stmts = []
+        with ZipFile(tmp_archive) as zipf:
+            logger.info(f'Extracting archive to {temp_extract}')
+            zipf.extractall(path=temp_extract)
+            logger.info('Processing jgif files')
+            for jgif in zipf.namelist():
+                if jgif.endswith('.jgf') or jgif.endswith('.jgif'):
+                    pbp = process_jgif_file(tmp_archive + jgif)
+                    stmts = stmts + pbp.statements
+
+        # Cleanup
+        shutil.rmtree(temp_extract)
+        os.remove(tmp_archive)
+
+        return stmts

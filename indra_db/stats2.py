a2b74644300a49b0da290ecdaf0cb775f1c8ebf5
pagreene <patrick.anton.greene@gmail.com>
2019-04-09 18:08:13 -0400

    Create function to generate x-y date for dates.

--- indra_db/stats2.py
+++ indra_db/stats2.py
@@ -1,4 +1,9 @@
+import sys
+import json
 from collections import defaultdict
+
+from indra_db.util import get_db
+
 
 def get_daily_counts(dates):
     counts = defaultdict(lambda: 0)
@@ -6,4 +11,38 @@ def get_daily_counts(dates):
         counts[date.date()] += 1
     return zip(*sorted(counts.items()))
 
+
+def get_all_daily_counts(db):
+    data_json = {}
+
+    # Daily update counts
+    for table in [db.TextRef, db.TextContent, db.Reading, db.RawStatements,
+                  db.PAStatements]:
+        if table == db.TextContent:
+            date_col = table.insert_date
+        else:
+            date_col = table.create_date
+        print('Processing daily dates for %s.' % table.__tablename__)
+        dates = [date for date, in db.select_all(date_col)]
+        t, n = get_daily_counts(dates)
+        data_json[table.__tablename__ + '_daily_counts'] = [t, n]
+
+    return
+
+def main(db_name):
+    db = get_db(db_name)
+    data_json = {}
+
+    data_json.update(get_all_daily_counts(db))
+
+    print('Dumping json...')
+    with open(db_name + '_stats.json', 'w') as f:
+        json.dumps(data_json, f, indent=2)
+
+    return
+
+
+if __name__ == '__main__':
+    db_name = sys.argv[1]
+    main(db_name)
 

eae0fce1a1e4b251cdc40a6ab5be8554a0ad1cc9
pagreene <patrick.anton.greene@gmail.com>
2019-07-17 09:38:04 -0400

    Clean up curationFunctions.js a bit.

--- rest_api/static/curationFunctions.js
+++ rest_api/static/curationFunctions.js
@@ -1,29 +1,29 @@
 // CURATION FUNCTIONS
 
 // Global constants
-var ALL_COLLAPSED = true;
+let ALL_COLLAPSED = true;
 
 // Force activate the sub items of the table of contents after page load
-$(document).ready(function(){
+$(document).ready(function () {
     $('a[href="#statements"]').addClass('active')
-})
+});
 
 // Variables
-var pubmed_fetch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
-var latestSubmission = {
-                        'ddSelect': '',
-                        'ev_hash': '',
-                        'source_hash': '',
-                        'submit_status': 0
-                    };
+let pubmed_fetch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
+let latestSubmission = {
+    'ddSelect': '',
+    'ev_hash': '',
+    'source_hash': '',
+    'submit_status': 0
+};
 
 // Check the API key input
-function keySubmit (key_value) {
-    var ensure_user = document.getElementById("ensure_user_on_api_key")
+function keySubmit(key_value) {
+    let ensure_user = document.getElementById("ensure_user_on_api_key");
     // Default value still there or nothing entered
     if (key_value == "No key given." | !key_value) {
         ensure_user.textContent = "No key given.";
-    // nothing entered
+        // nothing entered
     } else {
         console.log("document.getElementById(\"api_key_input\").value: " + document.getElementById("api_key_input").value);
         ensure_user.textContent = "Key stored!";
@@ -32,14 +32,14 @@ function keySubmit (key_value) {
 
 function submitButtonClick(clickEvent) {
     // // CURATOR
-    // var curator = document.getElementById("curator_input").value;
+    // let curator = document.getElementById("curator_input").value;
     // if (!curator) {
     //     alert("Please enter a curator ID");
     //     return;
     // }
 
     // // API KEY
-    // var api_key = document.getElementById("api_key_input").value;
+    // let api_key = document.getElementById("api_key_input").value;
     // if (!api_key) {
     //     alert("Please enter an API key!");
     //     return;
@@ -49,10 +49,10 @@ function submitButtonClick(clickEvent) {
     pn = clickEvent.target.parentNode.parentNode;
     btn_row_tag = pn.closest('tr');
     s = pn.getElementsByClassName("dropdown")[0]
-        .getElementsByTagName("select")[0]
+        .getElementsByTagName("select")[0];
 
     // DROPDOWN SELECTION
-    var err_select = s.options[s.selectedIndex].value;
+    let err_select = s.options[s.selectedIndex].value;
     if (!err_select) {
         alert('Please select an error type or "correct" for the statement in the dropdown menu');
         return;
@@ -60,31 +60,31 @@ function submitButtonClick(clickEvent) {
 
     // TEXT BOX CONTENT
     // Get "form" node, then the value of "input"
-    var user_text = pn
-                .getElementsByClassName("form")[0]
-                .getElementsByTagName("input")[0]
-                .value;
+    let user_text = pn
+        .getElementsByClassName("form")[0]
+        .getElementsByTagName("input")[0]
+        .value;
 
     // Refuse submission if 'other' is selected without providing a description
     if (!user_text & err_select == "other") {
-        alert('Must describe error when using option "other..."!')
+        alert('Must describe error when using option "other..."!');
         return;
     }
 
     // GET REFERENCE TO STATUS BOX (EMPTY UNTIL STATUS RECEIVED)
-    var statusBox = pn.getElementsByClassName("submission_status")[0].children[0];
+    let statusBox = pn.getElementsByClassName("submission_status")[0].children[0];
 
     // Step back to the preceding tr tag
-    var pmid_row = btn_row_tag.previousElementSibling;
+    let pmid_row = btn_row_tag.previousElementSibling;
 
     // PMID
     // Get pmid_linktext content
-    var pmid_text = pmid_row
-                .getElementsByClassName("pmid_link")[0]
-                .textContent.trim()
+    let pmid_text = pmid_row
+        .getElementsByClassName("pmid_link")[0]
+        .textContent.trim();
 
     // Icon 
-    var icon = pmid_row.getElementsByClassName("curation_toggle")[0]
+    let icon = pmid_row.getElementsByClassName("curation_toggle")[0];
 
     // HASHES: source_hash & stmt_hash
     // source_hash == ev['source_hash'] == pmid_row.id; "evidence level"
@@ -95,11 +95,11 @@ function submitButtonClick(clickEvent) {
     // CURATION DICT
     // example: curation_dict = {'tag': 'Reading', 'text': '"3200 A" is picked up as an agent.', 'curator': 'Klas', 'ev_hash': ev_hash};
     cur_dict = {
-                'tag': err_select, 
-                'text': user_text, 
-                // 'curator': curator,
-                'ev_hash': source_hash
-            };
+        'tag': err_select,
+        'text': user_text,
+        // 'curator': curator,
+        'ev_hash': source_hash
+    };
 
     // console.log("source hash: " + source_hash)
     // console.log("stmt hash: " + stmt_hash)
@@ -122,18 +122,16 @@ function submitButtonClick(clickEvent) {
         latestSubmission['ddSelect'] = err_select;
         latestSubmission['source_hash'] = source_hash;
         latestSubmission['stmt_hash'] = stmt_hash;
-    };
-
+    }
     testing = false; // Set to true to test the curation endpoint of the API
     ajx_response = submitCuration(cur_dict, stmt_hash, statusBox, icon, testing);
     console.log("ajax response from submission: ");
     console.log(ajx_response);
-};
-
+}
 // Submit curation
 function submitCuration(curation_dict, hash, statusBox, icon, test) {
     // API KEY FOR ACCESSING DATABASE
-    //var api_key = document.getElementById("api_key_input").value;
+    //let api_key = document.getElementById("api_key_input").value;
     //curation_addr = "https://db.indra.bio/curation/submit/";
     curation_addr = "http://localhost:5000/curation/submit/";
 
@@ -142,8 +140,7 @@ function submitCuration(curation_dict, hash, statusBox, icon, test) {
     if (test) {
         console.log("Submitting test curation...");
         _url += "&test";
-    };
-
+    }
     // console.log("api key: " + api_key)
     console.log("url: " + _url);
 
@@ -153,12 +150,12 @@ function submitCuration(curation_dict, hash, statusBox, icon, test) {
         dataType: "json",
         contentType: "application/json",
         data: JSON.stringify(curation_dict),
-        complete: function(xhr, statusText) {
+        complete: function (xhr, statusText) {
             latestSubmission['submit_status'] = xhr.status;
             switch (xhr.status) {
                 case 200:
                     statusBox.textContent = "Curation submitted successfully!";
-                    icon.style = "color: #00FF00" // Brightest green
+                    icon.style = "color: #00FF00"; // Brightest green
                     break;
                 case 400:
                     statusBox.textContent = xhr.status + ": Bad Curation Data";
@@ -177,9 +174,9 @@ function submitCuration(curation_dict, hash, statusBox, icon, test) {
                     icon.style = "color: #58D3F7"; // Icy blue
                     break;
                 default:
-                    console.log("Uncaught submission error: check ajax response")
-                    console.log("xhr: ")
-                    console.log(xhr)
+                    console.log("Uncaught submission error: check ajax response");
+                    console.log("xhr: ");
+                    console.log(xhr);
                     statusBox.textContent = "Uncaught submission error; Code " + xhr.status;
                     icon.style = "color: #FF8000"; // Warning orange
                     break;
@@ -187,8 +184,7 @@ function submitCuration(curation_dict, hash, statusBox, icon, test) {
         }
     });
     return response;
-};
-
+}
 // Creates the dropdown div with the following structure
 // <div class="dropdown" 
 //      style="display:inline-block; vertical-align: middle;">
@@ -210,21 +206,21 @@ function submitCuration(curation_dict, hash, statusBox, icon, test) {
 //         <option value="other">Other...</option>
 //     </select>
 // </div>
-function createDDDiv () {
-    var ddContainer = document.createElement("div")
-    ddContainer.className = "dropdown"
-    ddContainer.style = "display:inline-block; vertical-align: middle; margin-left: 9%;"
+function createDDDiv() {
+    let ddContainer = document.createElement("div");
+    ddContainer.className = "dropdown";
+    ddContainer.style = "display:inline-block; vertical-align: middle; margin-left: 9%;";
 
     let ddSelect = document.createElement("select");
 
     // DROPDOWN OPTIONS
     // Default; This is the option placeholder
-    placeholderOption = document.createElement("option");
+    const placeholderOption = document.createElement("option");
     placeholderOption.value = "";
     placeholderOption.selected = "selected";
     placeholderOption.disabled = "disabled";
     placeholderOption.hidden = "hidden";
-    placeholderOption.textContent = "Select error type..."
+    placeholderOption.textContent = "Select error type...";
     ddSelect.appendChild(placeholderOption);
     // 1 "correct" No Error;
     option1 = document.createElement("option");
@@ -289,8 +285,7 @@ function createDDDiv () {
     // Add more options by following the structure above
     ddContainer.appendChild(ddSelect);
     return ddContainer;
-};
-
+}
 // Creates the text box div with the following structure:
 // <div class="form" 
 //      style="display:inline-block; 
@@ -305,29 +300,28 @@ function createDDDiv () {
 //                style="width: 360px;">
 //     </form>
 // </div>
-function createTBDiv () {
-    var tbContainer = document.createElement("div")
-    tbContainer.className = "form"
-    tbContainer.style = "display:inline-block; vertical-align: middle; margin-left: 4%;"
+function createTBDiv() {
+    let tbContainer = document.createElement("div");
+    tbContainer.className = "form";
+    tbContainer.style = "display:inline-block; vertical-align: middle; margin-left: 4%;";
 
-    let tbForm = document.createElement("form")
-    tbForm.name = "user_feedback_form"
+    let tbForm = document.createElement("form");
+    tbForm.name = "user_feedback_form";
 
-    let tbInput = document.createElement("input")
+    let tbInput = document.createElement("input");
     tbInput.type = "text";
-    tbInput.maxlength = "240"
+    tbInput.maxlength = "240";
     tbInput.name = "user_feedback";
     tbInput.placeholder = "Optional description (240 chars)";
     tbInput.value = "";
-    tbInput.style = "width: 360px;"
+    tbInput.style = "width: 360px;";
 
     tbForm.appendChild(tbInput);
 
     tbContainer.appendChild(tbForm);
 
     return tbContainer;
-};
-
+}
 // Creates the submit button div with the following structure
 // <div class="curation_button"
 //      style="display:inline-block; 
@@ -347,8 +341,8 @@ function createTBDiv () {
 //     })
 //     </ script tag>
 // </div>
-function createSBDiv () {
-    var sbContainer = document.createElement("div");
+function createSBDiv() {
+    let sbContainer = document.createElement("div");
     sbContainer.className = "curation_button";
     sbContainer.style = "display:inline-block; vertical-align: middle; margin-left: 4%;";
 
@@ -362,16 +356,15 @@ function createSBDiv () {
     sbContainer.appendChild(sbButton);
 
     return sbContainer;
-};
-
+}
 // Creates the textbox that tells the user the status of the submission
 // <div class="submission_status"
 //      style="display:inline-block; 
 //             vertical-align: middle;">
 // <a class="submission_status"></a>
 // </div>
-function createStatusDiv () {
-    var statusContainer = document.createElement("div");
+function createStatusDiv() {
+    let statusContainer = document.createElement("div");
     statusContainer.className = "submission_status";
     statusContainer.style = "display:inline-block; vertical-align: middle; margin-left: 4%;";
 
@@ -389,85 +382,83 @@ function createStatusDiv () {
 // <td colspan="4" style="padding: 0px; border-top: 1px solid #FFFFFF;">
 function curationRowGenerator() {
     // Create new row element
-    var newRow = document.createElement('tr');
+    let newRow = document.createElement('tr');
     newRow.innerHTML = null;
     newRow.className = "cchild";
     newRow.style = "border-top: 1px solid #FFFFFF;";
 
     // Create new td element
     let newTD = document.createElement('td');
-    newTD.style="padding: 0px; border-top: 1px solid #FFFFFF; white-space: nowrap; text-align: left;";
+    newTD.style = "padding: 0px; border-top: 1px solid #FFFFFF; white-space: nowrap; text-align: left;";
     newTD.setAttribute("colspan", "4");
 
     // Add dropdown div
-    var dropdownDiv = createDDDiv();
+    let dropdownDiv = createDDDiv();
     newTD.appendChild(dropdownDiv);
     // Add textbox 
-    var textBoxDiv = createTBDiv();
+    let textBoxDiv = createTBDiv();
     newTD.appendChild(textBoxDiv);
     // Add submit button
-    var buttonDiv = createSBDiv();
+    let buttonDiv = createSBDiv();
     newTD.appendChild(buttonDiv);
     // Add submission response textbox
-    var statusDiv = createStatusDiv();
+    let statusDiv = createStatusDiv();
     newTD.appendChild(statusDiv);
 
     // Add td to table row
     newRow.appendChild(newTD);
 
     return newRow;
-};
-
+}
 // Adds the curation row to current
 function addCurationRow(clickedRow) {
     // Generate new row
-    var curationRow = curationRowGenerator();
+    let curationRow = curationRowGenerator();
 
     // Append new row to provided row
     clickedRow.parentNode.insertBefore(curationRow, clickedRow.nextSibling);
-};
-
+}
 function getPubMedMETAxmlByPMID(pmid) {
-    params_dict = {'db': 'pubmed',
+    let params_dict = {
+        'db': 'pubmed',
         'retmode': 'xml',
         'rettype': 'docsum',
         'id': pmid
     };
-    PubMedMETAxml = $.ajax({
+    return $.ajax({
         url: pubmed_fetch,
         type: "POST",
         dataType: "xml",
         data: params_dict,
     });
-    return PubMedMETAxml
-};
-
+}
 function pmidXML2dict(XML) {
-    xml_dict = {};
-    for (child of XML.children) {
-        name = child.getAttribute("Name");
-        type = child.getAttribute("Type");
-        if (child.hasChildNodes() & type == "List") {
+    let xml_dict = {};
+    for (let child of XML.children) {
+        let name = child.getAttribute("Name");
+        let type = child.getAttribute("Type");
+        if (child.hasChildNodes() && type === "List") {
+            let innerItems;
             // Javascript can't really do nice recursive functions...
             // special cases for "History" and "ArticleIds" which has unique inner Names
-            if (name == "ArticleIds" | name == "History") {
-                innerDict = {};
+            if (name === "ArticleIds" || name === "History") {
+                let innerDict = {};
                 for (c of child.children) {
                     innerDict[c.getAttribute("Name")] = c.textContent;
                 }
                 innerItems = innerDict;
             } else {
-                innerList = [];
+                let innerList = [];
                 for (c of child.children) {
                     innerList.push(c.textContent);
                 }
                 innerItems = innerList;
             }
             xml_dict[name] = innerItems
-        } else if (child.tagName == "Item") {
+        } else if (child.tagName === "Item") {
             // Here just get the inner strings
             xml_dict[name] = child.textContent;
-        } else if (child.tagName == "Id") {
+        } else if (child.tagName === "Id") {
             // Special case
             xml_dict["Id"] = child.textContent;
         } else {
@@ -484,28 +475,24 @@ function pmidXML2dict(XML) {
 // Modify link hover text
 function setPMIDlinkTitle(pmid, link_tag) {
     let pubmed_xml_promise = getPubMedMETAxmlByPMID(pmid);
-    pubmed_xml_promise.then(function(responseXML) {
-        docsum_xml = responseXML.getElementsByTagName('DocSum')[0];
-        pmid_meta_dict = pmidXML2dict(docsum_xml);
-        authorlist = pmid_meta_dict.AuthorList
-        if (authorlist.length > 3) {
-            authors = authorlist[0] + ", ... " + authorlist[authorlist.length-1];
-        } else {
-            authors = authorlist.join(", ");
-        }
-        // Shortened journal name is in .Source, while full name is in .FullJournalName
-        journal = pmid_meta_dict.Source
-        SO = pmid_meta_dict.SO
-        title = pmid_meta_dict.Title
+    pubmed_xml_promise.then(responseXML => {
+        const docsum_xml = responseXML.getElementsByTagName('DocSum')[0];
+        const pmd = pmidXML2dict(docsum_xml);
+        const nAuthors = pmd.AuthorList.length;
+        let authorsStr;
+        if (nAuthors > 3)
+            authorsStr = `${pmd.AuthorList[0]}, ... ${pmc.AuthorList[nAuthors-1]}`;
+        else
+            authorsStr = pmd.AuthorList.join(", ");
 
-        pmid_hover_string = authors + ", \"" + title + "\", " + journal + ", " + SO
-        link_tag.title = pmid_hover_string;
+        // Shortened journal name is in .Source, while full name is in .FullJournalName
+        link_tag.title = `${authorsStr}, "${pmd.Title}", ${pmd.Source}, ${pmd.SO}`;
     })
 }
 
 // Loop all pmid link nodes and set title
 function populatePMIDlinkTitles() {
-    var pmid_link_array = document.getElementsByClassName("pmid_link");
+    let pmid_link_array = document.getElementsByClassName("pmid_link");
     for (link_obj of pmid_link_array) {
         pmid = link_obj.textContent;
         setPMIDlinkTitle(pmid, link_obj)
@@ -514,15 +501,15 @@ function populatePMIDlinkTitles() {
 
 // Expand/collapse all
 function expandCollapseAll() {
-    var expColBtn = document.getElementById('expand-collapse-all')
-    var setCss = ''
+    let expColBtn = document.getElementById('expand-collapse-all');
+    let setCss = '';
 
     // Expand all; set ALL_COLLAPSED = false;
     if (ALL_COLLAPSED) {
-        setCss = 'display: block;'
+        setCss = 'display: block;';
         ALL_COLLAPSED = false;
         expColBtn.textContent = 'Collapse All';
-    // Collapse all; set ALL_COLLAPSED = true;
+        // Collapse all; set ALL_COLLAPSED = true;
     } else {
         setCss = 'display: none;';
         ALL_COLLAPSED = true;
@@ -530,26 +517,26 @@ function expandCollapseAll() {
     }
 
     // Loop all tags
-    for ( tag of document.getElementsByClassName('group') ) {
+    for (tag of document.getElementsByClassName('group')) {
         tag.style.cssText = setCss
     }
 }
 
 // Expand/collapse row
-$(function() {
-    $("td[class='curation_toggle']").click(function(event) {
+$(function () {
+    $("td[class='curation_toggle']").click(function (event) {
         event.stopPropagation();
-        var $target = $(event.target);
+        let $target = $(event.target);
         if (event.target.dataset.clicked == "true") {
             // Toggle (animation duration in msec)
             $target.closest("tr").next().find("div").slideToggle(200);
-        // First click event
+            // First click event
         } else {
             // Stay down (animation duration in msec)
             $target.closest("tr").next().find("div").slideDown(400);
 
             // Change color of icon to light gray
-            event.target.style="color:#A4A4A4;"
+            event.target.style = "color:#A4A4A4;";
 
             // Set clicked to true
             event.target.dataset.clicked = "true"

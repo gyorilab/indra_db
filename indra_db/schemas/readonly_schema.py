5bd4458a93b3efac6ed4afe20a7f442dd55c426f
pagreene <patrick.anton.greene@gmail.com>
2019-09-11 10:55:55 -0400

    Set schema name as prefix to former mv names.

--- indra_db/schemas/readonly_schema.py
+++ indra_db/schemas/readonly_schema.py
@@ -38,7 +38,7 @@ def get_schema(Base):
     read_views = {}
 
     class EvidenceCounts(Base, MaterializedView):
-        __tablename__ = 'evidence_counts'
+        __tablename__ = 'readonly.evidence_counts'
         __definition__ = ('SELECT count(id) AS ev_count, mk_hash '
                           'FROM fast_raw_pa_link '
                           'GROUP BY mk_hash')
@@ -47,7 +47,7 @@ def get_schema(Base):
     read_views[EvidenceCounts.__tablename__] = EvidenceCounts
 
     class ReadingRefLink(Base, MaterializedView):
-        __tablename__ = 'reading_ref_link'
+        __tablename__ = 'readonly.reading_ref_link'
         __definition__ = ('SELECT pmid, pmcid, tr.id AS trid, doi, '
                           'pii, url, manuscript_id, tc.id AS tcid, '
                           'source, r.id AS rid, reader '
@@ -75,7 +75,7 @@ def get_schema(Base):
     read_views[ReadingRefLink.__tablename__] = ReadingRefLink
 
     class FastRawPaLink(Base, MaterializedView):
-        __tablename__ = 'fast_raw_pa_link'
+        __tablename__ = 'readonly.fast_raw_pa_link'
         __definition__ = ('SELECT raw.id AS id, raw.json AS raw_json, '
                           'raw.reading_id, raw.db_info_id, '
                           'pa.mk_hash, pa.json AS pa_json, pa.type '
@@ -98,7 +98,7 @@ def get_schema(Base):
     read_views[FastRawPaLink.__tablename__] = FastRawPaLink
 
     class PaMeta(Base, MaterializedView):
-        __tablename__ = 'pa_meta'
+        __tablename__ = 'readonly.pa_meta'
         __definition__ = ('SELECT pa_agents.db_name, pa_agents.db_id, '
                           'pa_agents.id AS ag_id, pa_agents.role, '
                           'pa_agents.ag_num, pa_statements.type, '
@@ -121,7 +121,7 @@ def get_schema(Base):
     read_views[PaMeta.__tablename__] = PaMeta
 
     class TextMeta(Base, NamespaceLookup):
-        __tablename__ = 'text_meta'
+        __tablename__ = 'readonly.text_meta'
         __dbname__ = 'TEXT'
         _indices = [StringIndex('text_meta_db_id_idx', 'db_id'),
                     StringIndex('text_meta_type_idx', 'type')]
@@ -136,7 +136,7 @@ def get_schema(Base):
     read_views[TextMeta.__tablename__] = TextMeta
 
     class NameMeta(Base, NamespaceLookup):
-        __tablename__ = 'name_meta'
+        __tablename__ = 'readonly.name_meta'
         __dbname__ = 'NAME'
         _indices = [StringIndex('name_meta_db_id_idx', 'db_id'),
                     StringIndex('name_meta_type_idx', 'type')]
@@ -151,7 +151,7 @@ def get_schema(Base):
     read_views[NameMeta.__tablename__] = NameMeta
 
     class OtherMeta(Base, MaterializedView):
-        __tablename__ = 'other_meta'
+        __tablename__ = 'readonly.other_meta'
         __definition__ = ("SELECT db_name, db_id, ag_id, role, ag_num, "
                           "type, mk_hash, ev_count FROM pa_meta "
                           "WHERE db_name NOT IN ('NAME', 'TEXT')")
@@ -170,7 +170,7 @@ def get_schema(Base):
     read_views[OtherMeta.__tablename__] = OtherMeta
 
     class RawStmtSrc(Base, MaterializedView):
-        __tablename__ = 'raw_stmt_src'
+        __tablename__ = 'readonly.raw_stmt_src'
         __definition__ = ('SELECT raw_statements.id AS sid, '
                           'lower(reading.reader) AS src '
                           'FROM raw_statements, reading '
@@ -185,7 +185,7 @@ def get_schema(Base):
     read_views[RawStmtSrc.__tablename__] = RawStmtSrc
 
     class PaStmtSrc(Base, MaterializedView):
-        __tablename__ = 'pa_stmt_src'
+        __tablename__ = 'readonly.pa_stmt_src'
         __definition_fmt__ = ("SELECT * FROM crosstab("
                               "'SELECT mk_hash, src, count(sid) "
                               "  FROM raw_stmt_src "

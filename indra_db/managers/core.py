e4ecbc5fc30ba836fe730350b30d0b9085ce6ccb
pagreene <patrick.anton.greene@gmail.com>
2020-01-15 14:25:16 -0500

    Refactor core completely.

--- indra_db/managers/core.py
+++ indra_db/managers/core.py
@@ -2,81 +2,104 @@ import functools
 import json
 import logging
 import traceback
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import boto3
 
 logger = logging.getLogger(__name__)
 
 # TODO: Make this a config var
-S3_DATA_LOC = {'bucket': 'bigmech', 'prefix': 'indra-db/manager-data/'}
-RESERVED_KEYS = ['error_info', 'start_time', 'end_time']
+S3_DATA_LOC = {'bucket': 'bigmech', 'prefix': 'indra-db/managers/'}
 DATE_FMT = '%Y%m%d_%H%M%S'
 
 
-class Manager(object):
-    data_inits = NotImplemented
-    reserved_data_inits = {
-        'error_info': dict,
-        'end_time': str,
-        'start_time': str,
-        'duration': int
-    }
-    stage_name = NotImplemented
-
-    def __init__(self):
-        self.stats = {data_name: data_type()
-                      for data_name, data_type in self.data_inits.items()}
-
-    def get_stat_file_key(self):
-        s3_suffix = '%s/%s.json' % (self.date_id, self.stage_name)
-        s3_key = S3_DATA_LOC['prefix'] + s3_suffix
-        return s3_key
-
-    def get_stats(self):
-        return self.stats.copy()
-
-
-class ManagerEnv(object):
-    @classmethod
-    def wrap(cls, func):
-        @functools.wraps(func)
-        def decorated(obj, *args, **kwargs):
-            with cls(obj):
-                return func(obj, *args, **kwargs)
-        return decorated
-
-    def __init__(self, manager):
-        self.manager = manager
+class DGContext(object):
+    def __init__(self, gatherer):
+        self.gatherer = gatherer
 
     def __enter__(self):
-        self.start_time = datetime.utcnow()
-        logger.info("Entering manager env.")
+        self.gatherer.start()
 
-    def __exit__(self, err_type, err, tb):
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.gatherer.dump(exc_type, exc_val, exc_tb)
+
+    @classmethod
+    def wrap(cls, gatherer):
+        def sub_wrap(func):
+            @functools.wraps(func)
+            def decorated(*args, **kwargs):
+                with cls(gatherer):
+                    return func(*args, **kwargs)
+            return decorated
+        return sub_wrap
+
+
+class DataGatherer(object):
+    def __init__(self, label, counts_fields):
+        self._label = label
+        self._sub_label = None
+        self._counts_fields = counts_fields
+        self._timing = self._counts = self._error = None
+        return
+
+    def set_sub_label(self, sub_label):
+        self._sub_label = sub_label
+        return
+
+    def start(self):
+        self._timing = {
+            'start': datetime.utcnow(),
+            'end': None,
+            'dur': None
+        }
+        self._counts = dict.fromkeys(self._counts_fields, 0)
+        return
+
+    def add(self, field, num=1):
+        if field not in self._counts:
+            raise ValueError('Unexpected field: %s. Should be one of: %s.'
+                             % (field, self._counts_fields))
+        self._counts[field] += num
+        return
+
+    def dump(self, err_type, err, tb):
         logger.info("Leaving manager env with error type: %s" % err_type)
         s3 = boto3.client('s3')
 
+        if err_type:
+            err_msg_lines = traceback.format_exception(err_type, err, tb)
+            self._error = {
+                'type': err_type.__name__,
+                'value': str(err),
+                'traceback': ''.join(err_msg_lines)
+            }
+
+        self._timing['end'] = datetime.utcnow()
+        self._timing['dur'] = self._timing['end'] - self._timing['start']
+
         # Get the s3 key from the manager
-        key = self.manager.get_stat_file_key()
+        key = S3_DATA_LOC['prefix'] + self._label
+        if self._sub_label:
+            key += '/' + self._sub_label
+        key += '.json'
 
         # Get the stats from the manager
-        stats = self.manager.get_stats()
+        stats = {'timing': self.make_timing_json(),
+                 'counts': self._counts,
+                 'error': self._error}
         s3.put_object(Bucket=S3_DATA_LOC['bucket'], Key=key,
                       Body=json.dumps(stats))
+        return
 
-        # Add some generic stats.
-        gen_stats = {}
-        err_info = {
-            'error_type': str(err_type),
-            'error_value': str(err),
-            'traceback': ''.join(traceback.format_exception(err_type, err, tb))
-        }
-        gen_stats['error_info'] = err_info
-        self.end_time = datetime.utcnow()
-        gen_stats['end_time'] = self.end_time.strftime(DATE_FMT)
-        gen_stats['start_time'] = self.manager.date_id
-
-        # Dump the gen_stats on s3.
-        s3.put_object(Bucket=S3_DATA_LOC['bucket'], Key=key,
-                      Body=json.dumps(gen_stats))
+    def make_timing_json(self):
+        timing_res = {}
+        for name, value in self._timing.items():
+            if isinstance(value, datetime):
+                timing_res[name] = value.timestamp()*1000
+            elif isinstance(value, timedelta):
+                timing_res[name] = value.total_seconds()*1000
+            else:
+                logger.warning("Expected datetime related object, but got %s."
+                               % type(value))
+                timing_res[name] = value
+        return timing_res

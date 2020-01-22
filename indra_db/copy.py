d91e08dc00d1e5ad0ca79d503bc81318061567a4
pagreene <patrick.anton.greene@gmail.com>
2020-01-22 14:16:26 -0500

    Implement push_report copy.

--- indra_db/copy.py
+++ indra_db/copy.py
@@ -11,17 +11,16 @@ logger = logging.getLogger(__name__)
 
 class LazyCopyManager(CopyManager):
     """A copy manager that ignores entries which violate constraints."""
-    base_cmd_fmt = ('CREATE TEMP TABLE "tmp_{table}" '
-                    'ON COMMIT DROP '
-                    'AS SELECT "{cols}" FROM "{schema}"."{table}" '
-                    'WITH NO DATA; '
-                    '\n'
-                    'COPY "tmp_{table}" ("{cols}") '
-                    'FROM STDIN WITH BINARY; '
-                    '\n'
-                    'INSERT INTO "{schema}"."{table}" ("{cols}") '
-                    'SELECT "{cols}" '
-                    'FROM "tmp_{table}" ON CONFLICT ')
+    _fill_tmp_fmt = ('CREATE TEMP TABLE "tmp_{table}"\n'
+                     'ON COMMIT DROP\n'
+                     'AS SELECT "{cols}" FROM "{schema}"."{table}"\n'
+                     'WITH NO DATA;\n'
+                     'COPY "tmp_{table}" ("{cols}")\n'
+                     'FROM STDIN WITH BINARY;')
+
+    _merge_fmt = ('INSERT INTO "{schema}"."{table}" ("{cols}")\n'
+                  'SELECT "{cols}"\n'
+                  'FROM "tmp_{table}" ON CONFLICT ')
 
     def __init__(self, conn, table, cols, constraint=None):
         super().__init__(conn, table, cols)
@@ -29,27 +28,26 @@ class LazyCopyManager(CopyManager):
         return
 
     def report_copy(self, data, order_by=None, return_cols=None,
-                    compare_cols=None, fobject_factory=tempfile.TemporaryFile):
-        datastream = fobject_factory()
-        self.writestream(data, datastream)
-        datastream.seek(0)
-        self.copystream(datastream)
-        datastream.close()
-
-        res = self._get_skipped(len(data), order_by, return_cols, compare_cols)
-        return res
+                    fobject_factory=tempfile.TemporaryFile):
+        self.copy(data, fobject_factory)
+        return self._get_skipped(len(data), order_by, return_cols)
 
     def _stringify_cols(self, cols):
         return '", "'.join(cols)
 
-    def _get_sql_fmt(self):
-        cmd_fmt = self.base_cmd_fmt
+    def _get_sql(self):
+        cmd_fmt = '\n'.join([self._fill_tmp_fmt, self._merge_fmt])
         if self.constraint:
             cmd_fmt += 'ON CONSTRAINT "%s" ' % self.constraint
         cmd_fmt += 'DO NOTHING;\n'
-        return cmd_fmt
 
-    def _get_skipped(self, num, order_by, return_cols=None, compare_cols=None):
+        # Fill in the format.
+        columns = self._stringify_cols(self.cols)
+        sql = cmd_fmt.format(schema=self.schema, table=self.table,
+                             cols=columns)
+        return sql
+
+    def _get_skipped(self, num, order_by, return_cols=None):
         cursor = self.conn.cursor()
         inp_cols = self._stringify_cols(self.cols)
         if return_cols:
@@ -58,12 +56,12 @@ class LazyCopyManager(CopyManager):
             ret_cols = inp_cols
         diff_sql = (
             'SELECT "{ret_cols}" FROM\n'
-            '"tmp_{table}"\n'
-            'EXCEPT\n'
-            '(SELECT "{cols}"\n'
-            ' FROM "{schema}"."{table}"\n'
-            ' ORDER BY "{order_by}" DESC\n'
-            ' LIMIT {num});'
+            '(SELECT "{cols}" FROM "tmp_{table}"\n'
+            ' EXCEPT\n'
+            ' (SELECT "{cols}"\n'
+            '  FROM "{schema}"."{table}"\n'
+            '  ORDER BY "{order_by}" DESC\n'
+            '  LIMIT {num})) as t;'
         ).format(cols=inp_cols, table=self.table, schema=self.schema,
                  ret_cols=ret_cols, order_by=order_by, num=num)
         logger.debug(diff_sql)
@@ -72,13 +70,8 @@ class LazyCopyManager(CopyManager):
         return res
 
     def copystream(self, datastream):
-        cmd_fmt = self._get_sql_fmt()
+        sql = self._get_sql()
 
-        # Fill in the format.
-        columns = self._stringify_cols(self.cols)
-        sql = cmd_fmt.format(schema=self.schema,
-                             table=self.table,
-                             cols=columns)
         logger.debug(sql)
         cursor = self.conn.cursor()
         res = None
@@ -92,31 +85,63 @@ class LazyCopyManager(CopyManager):
 
 
 class PushCopyManager(LazyCopyManager):
+    _end_table_fmt = ('CREATE TEMP TABLE "end_{table}" '
+                      'ON COMMIT DROP '
+                      'AS SELECT "{order_by}" FROM "{schema}"."{table}" '
+                      'ORDER BY "{order_by}" DESC LIMIT 1; ')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not self.constraint:
             raise ValueError("A constraint is required if you are updating "
                              "on-conflict.")
+        self.reporting = False
+        self.order_by = None
+        return
 
-    def _get_sql_fmt(self):
-        cmd_fmt = self.base_cmd_fmt
+    def _get_sql(self):
+        if self.reporting:
+            cmd_fmt = '\n'.join([self._fill_tmp_fmt, self._end_table_fmt,
+                                 self._merge_fmt])
+        else:
+            cmd_fmt = '\n'.join([self._fill_tmp_fmt, self._merge_fmt])
         update = ', '.join('{0} = EXCLUDED.{0}'.format(c)
                            for c in self.cols)
         cmd_fmt += 'ON CONSTRAINT "%s" DO UPDATE SET %s;\n' \
                    % (self.constraint, update)
-        return cmd_fmt
+        columns = self._stringify_cols(self.cols)
+        sql = cmd_fmt.format(schema=self.schema, table=self.table,
+                             cols=columns, order_by=self.order_by)
+        return sql
 
-    def _get_skipped(self, num, order_by, return_cols=None, compare_cols=None):
-        if not compare_cols:
-            raise ValueError("")
-        # TODO: Get the compare columns automatically at this point?
-        # Query to get constraint info:
-        # SELECT con.*
-        #        FROM pg_catalog.pg_constraint con
-        #             INNER JOIN pg_catalog.pg_class rel
-        #                        ON rel.oid = con.conrelid
-        #             INNER JOIN pg_catalog.pg_namespace nsp
-        #                        ON nsp.oid = connamespace
-        #        WHERE nsp.nspname = '<schema name>'
-        #              AND rel.relname = '<table name>';
-        pass
+    def _get_report(self, return_cols=None):
+        cursor = self.conn.cursor()
+        inp_cols = self._stringify_cols(self.cols)
+        if return_cols:
+            ret_cols = self._stringify_cols(return_cols)
+        else:
+            ret_cols = inp_cols
+        diff_sql = (
+            'SELECT "{ret_cols}"\n'
+            'FROM\n'
+            '(SELECT "{cols}" FROM "tmp_{table}"\n'
+            ' EXCEPT\n'
+            ' (SELECT "{cols}"\n'
+            '  FROM "{schema}"."{table}"\n'
+            '  WHERE "{order_by}" > (SELECT "{order_by}" '
+            '                        FROM "end_{table}"))) AS t;'
+        ).format(ret_cols=ret_cols, order_by=self.order_by, table=self.table,
+                 schema=self.schema, cols=inp_cols)
+        logger.debug(diff_sql)
+        cursor.execute(diff_sql)
+        res = cursor.fetchall()
+        return res
+
+    def report_copy(self, data, order_by=None, return_cols=None,
+                    fobject_factory=tempfile.TemporaryFile):
+        self.reporting = True
+        self.order_by = order_by
+        self.copy(data, fobject_factory)
+        updated = self._get_report(return_cols)
+        return updated
+

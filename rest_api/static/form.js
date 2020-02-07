0aab2d8d9c4885741799d342f2cc9559e711786c
Patrick Greene <patrick.anton.greene@gmail.com>
2020-02-07 13:04:41 -0500

    Transition to select after groudning.

--- rest_api/static/form.js
+++ rest_api/static/form.js
@@ -1,17 +1,20 @@
 Vue.component('stmt-search', {
   template: `
     <div class='stmt_search'>
-      <div>
+      <div v-if="!options">
         <input v-model="agent" placeholder="Enter agent here">
         <button @click='lookupOptions'>Ground</button>
         <span v-show='searching'>Searching...</span>
       </div>
-      <div v-if="options">
-        Gilda Options:
-        <div v-for="(option, option_idx) in options" :key="option_idx">
-          <hr>
-          {{ option.term.entry_name }} (score: {{ option.score.toFixed(2) }}, {{ option.term.status }} from {{ option.term.source }})
-        </div>
+      <div v-else>
+        <select v-model='selected_option'>
+          <option value='' selected disabled hidden>Select grounding option...</option>
+          <option v-for='(option, option_idx) in options'
+                  :key='option_idx'
+                  :value='option_idx'>
+            <b>{{ option.term.entry_name }}</b> (score: {{ option.score.toFixed(2) }}, {{ option.term.status }} from {{ option.term.source }})
+          </option>
+        </select>
       </div>
     </div>
   `,

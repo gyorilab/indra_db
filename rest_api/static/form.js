53e4b4de74cda499a12eb31090b3bf23b08d395b
Patrick Greene <patrick.anton.greene@gmail.com>
2020-02-07 16:36:30 -0500

    Display options only if there are multiple.

--- rest_api/static/form.js
+++ rest_api/static/form.js
@@ -1,3 +1,15 @@
+Vue.component('grounding-option', {
+  template: `
+    <span class='grounding-option'>
+      <b>{{ term.entry_name }}</b> (score: {{ score.toFixed(2) }}, {{ term.status }} from {{ term.source }})
+    </span>`,
+  props: [
+    'term',
+    'score',
+  ]
+});
+
+
 Vue.component('stmt-search', {
   template: `
     <div class='stmt_search'>
@@ -6,14 +18,19 @@ Vue.component('stmt-search', {
         <button @click='lookupOptions'>Ground</button>
         <span v-show='searching'>Searching...</span>
       </div>
+      <div v-else-if="options.length == 1">
+        <span class='frozen-box'>
+          <grounding-option v-bind="options[0]"></grounding-option>
+        </span>
+        <button @click='resetOptions'>Cancel</button>
+      </div>
       <div v-else>
         <select v-model='selected_option'>
-          <option v-if="options.length > 1" value='' selected disabled hidden>Select grounding option...</option>
+          <option value='' selected disabled hidden>Select grounding option...</option>
           <option v-for='(option, option_idx) in options'
                   :key='option_idx'
-                  :value='option_idx'
-                  :selected="options.length > 1">
-            <b>{{ option.term.entry_name }}</b> (score: {{ option.score.toFixed(2) }}, {{ option.term.status }} from {{ option.term.source }})
+                  :value='option_idx'>
+            <grounding-option v-bind='option'></grounding-option>
           </option>
         </select>
         <button @click='resetOptions'>Cancel</button>

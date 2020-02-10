7633b17368c2365c05bdce118593a727083b5c5e
Patrick Greene <patrick.anton.greene@gmail.com>
2020-02-10 13:06:39 -0500

    Fix empty stmt_type handling.

--- rest_api/static/form.js
+++ rest_api/static/form.js
@@ -81,7 +81,7 @@ Vue.component('agent-select', {
 Vue.component('stmt-search', {
   template: `
     <div class='stmt_search'>
-      <div id='seach-box' v-if='show_search'>
+      <div id='seach-box' v-show='show_search'>
           <h3>Select Agents</h3>
           <div v-for="(agent, agent_idx) in agents"
                :key='agent_idx'>
@@ -103,7 +103,7 @@ Vue.component('stmt-search', {
           <h3>Search</h3>
           <button @click='search'>Search</button>
       </div>
-      <div v-else>
+      <div v-show='!show_search'>
         <span id='search-reopen' @click='show_search=true'>Search...</span>
       </div>
       
@@ -149,23 +149,32 @@ Vue.component('stmt-search', {
       // Format the agents into the query.
       let tagged_ag, term, role;
       for (let idx in this.agents) {
+        window.console.log(idx)
         term = this.agents[idx].grounding.term;
         role = this.agents[idx].role
-        if (idx !== 0)
+        if (idx != 0) {
+          window.console.log(idx, idx != 0);
           query_str += '&'
+        }
       
         tagged_ag = term.id + '@' + term.db;
         if (role === 'none')
           query_str += `agent${idx}=${tagged_ag}`;
         else
           query_str += `${role}=${tagged_ag}`;
+
+        window.console.log(query_str)
       }
 
       // Format the statement type into the query.
-      if (this.stmt_type)
-        if (query_str)
-          query_str += '&'
-        query_str += `stmt_type=${this.stmt_type}`
+      if (this.stmt_type !== null) {
+        if (this.stmt_type.trim()) {
+          if (query_str)
+            query_str += '&'
+          query_str += `stmt_type=${this.stmt_type}`
+          window.console.log(query_str);
+        }
+      }
 
       // Make the query
       let url = `${this.$search_url}?limit=10&${query_str}`

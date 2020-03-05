577c97cc1c0bc6adb1eff977a44236cf13ef8a38
pagreene <patrick.anton.greene@gmail.com>
2020-03-05 14:39:27 -0500

    Handle stagger option.

--- indra_db/reading/submit_reading_pipeline.py
+++ indra_db/reading/submit_reading_pipeline.py
@@ -17,10 +17,13 @@ bash$ python submit_reading_pipeline.py --help
 In your favorite command line.
 """
 import json
+from asyncio import sleep
+
 import boto3
 import logging
 import botocore
 from datetime import datetime
+from functools import partial
 
 from indra_reading.util import get_s3_log_prefix
 from indra_reading.scripts.submit_reading_pipeline import create_submit_parser,\
@@ -240,11 +243,26 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+    logger.info("Reading for %s" % args.readers)
     sub = DbReadingSubmitter(args.basename, args.readers, args.project)
     sub.set_options(args.stmt_mode, args.read_mode,
                     args.max_reach_input_len, args.max_reach_space_ratio)
-    sub.submit_reading(args.input_file, args.start_ix, args.end_ix,
-                       args.ids_per_job)
+
+    submit = partial(sub.submit_reading, args.input_file, args.start_ix,
+                     args.end_ix, args.ids_per_job, stagger=args.stagger)
+    if args.stagger > 0:
+        from threading import Thread
+        th = Thread(target=submit)
+        th.start()
+        while not sub.monitors:
+            sleep(1)
+            logger.info("Waiting for monitors...")
+    else:
+        submit()
+
     if not args.no_wait:
         sub.watch_and_wait(idle_log_timeout=args.idle_log_timeout,
                            kill_on_timeout=not args.no_kill_on_timeout)
+
+    if args.stagger > 0:
+        th.join()

86c0e91795f07d4298dc48ac959f5e4a56f13644
pagreene <patrick.anton.greene@gmail.com>
2020-03-10 16:28:37 -0400

    Remove indrlab vue serve endpoint.

--- rest_api/api.py
+++ rest_api/api.py
@@ -246,25 +246,6 @@ def serve_data_vis(file_path):
                         content_type=ct)
 
 
-@app.route('/indralab-vue/<file>')
-def serve_indralab_vue(file):
-    full_path = path.join(HERE, '..', '..', 'indralab-vue/dist', file)
-    logger.info('indralab-vue: ' + full_path)
-    if not path.exists(full_path):
-        abort(404)
-        return
-    ext = full_path.split('.')[-1]
-    if ext == 'js':
-        ct = 'application/javascript'
-    elif ext == 'css':
-        ct = 'text/css'
-    else:
-        ct = None
-    with open(full_path, 'rb') as f:
-        return Response(f.read(),
-                        content_type=ct)
-
-
 @app.route('/monitor')
 def get_data_explorer():
     return render_my_template('daily_data.html', 'Monitor')

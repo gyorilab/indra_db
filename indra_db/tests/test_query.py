078c157cb786313e60159478fd640ec51cf80e98
pagreene <patrick.anton.greene@gmail.com>
2020-04-02 10:00:46 -0400

    Refactor test.

--- indra_db/tests/test_query.py
+++ indra_db/tests/test_query.py
@@ -5,8 +5,26 @@ from indra_db.util import get_db
 from indra_db.client.query import *
 
 
-def test_all_queries():
-    db = get_db('primary')
+db = get_db('primary')
+
+
+def dq(q):
+    print('---------------------------')
+    print(q)
+    print('---------------------------')
+    print(q._get_mk_hashes_query(db))
+    print('---------------------------')
+    start = datetime.now()
+    # res = q.get_statements(db, limit=10, ev_limit=2)
+    res = q.get_hashes(db)
+    print(f'Number of hashes: {len(res.results)}')
+    end = datetime.now()
+    print(f'Duration: {end - start}')
+    print('\n================================================\n')
+    return res.results
+
+
+def test_query_set_behavior():
 
     queries = [
         AgentQuery('TP53', role='SUBJECT'),
@@ -23,23 +41,6 @@ def test_all_queries():
         TypeQuery(['RegulateActivity'], include_subclasses=True),
         TypeQuery(['Complex'])
     ]
-
-
-    def dq(q):
-        print('---------------------------')
-        print(q)
-        print('---------------------------')
-        print(q._get_mk_hashes_query(db))
-        print('---------------------------')
-        start = datetime.now()
-        # res = q.get_statements(db, limit=10, ev_limit=2)
-        res = q.get_hashes(db)
-        print(f'Number of hashes: {len(res.results)}')
-        end = datetime.now()
-        print(f'Duration: {end - start}')
-        print('\n================================================\n')
-        return res.results
-
 
     results = []
     for q in queries:

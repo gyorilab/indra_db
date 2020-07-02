a05fdf537eac992e1d44a6a74cf1737bf64e41ca
pagreene <patrick.anton.greene@gmail.com>
2020-07-02 09:13:53 -0400

    Remove pop function, promote db_query builder to method.

--- rest_api/api.py
+++ rest_api/api.py
@@ -259,6 +259,10 @@ class ApiCall:
 
 
 class StatementApiCall(ApiCall):
+    def __init__(self):
+        super(StatementApiCall, self).__init__()
+        self.strict = self._pop('strict', False, bool)
+        return
 
     def get_db_query(self):
         raise NotImplementedError()
@@ -329,6 +333,92 @@ class StatementApiCall(ApiCall):
                        sec_since(self.start_time)))
         return resp
 
+    def _db_query_from_web_query(self, require_any=None, require_all=None,
+                                 empty_web_query=False):
+        db_query = EmptyQuery()
+        num_agents = 0
+
+        logger.info(f"Making DB query from:\n{self.web_query}")
+        filter_ev = self._pop('filter_ev', False, bool)
+        ev_filter = EvidenceFilter()
+
+        # Get the agents without specified locations (subject or object).
+        for raw_ag in iter_free_agents(self.web_query):
+            ag, ns = process_agent(raw_ag)
+            db_query &= HasAgent(ag, namespace=ns)
+
+        # Get the agents with specified roles.
+        for role in ['subject', 'object']:
+            raw_ag = self._pop(role, None)
+            if raw_ag is None:
+                continue
+            if isinstance(raw_ag, list):
+                assert len(raw_ag) == 1, f'Malformed agent for {role}: {raw_ag}'
+                raw_ag = raw_ag[0]
+            num_agents += 1
+            ag, ns = process_agent(raw_ag)
+            db_query &= HasAgent(ag, namespace=ns, role=role.upper())
+
+        # Get the raw name of the statement type (we fix some variations).
+        act_raw = self._pop('type', None)
+        if act_raw is not None:
+            if isinstance(act_raw, list):
+                assert len(act_raw) == 1, \
+                    f"Got multiple entries for statement type: {act_raw}."
+                act_raw = act_raw[0]
+            act = make_statement_camel(act_raw)
+            db_query &= HasType([act])
+
+        # Get whether the user wants a strict match
+        if self.strict:
+            db_query &= HasNumAgents((num_agents,))
+
+        # Unpack hashes, if present.
+        hashes = self._pop('hashes', None)
+        if hashes:
+            db_query &= HasHash(hashes)
+
+        # Unpack paper ids, if present:
+        id_tpls = set()
+        for id_str in self._pop('paper_ids', []):
+            val, typ = id_str.split('@')
+
+            # Turn tcids and trids into integers.
+            id_val = int(val) if typ in ['tcid', 'trid'] else val
+            id_tpls.add((typ, id_val))
+
+        if id_tpls:
+            paper_q = FromPapers(id_tpls)
+            db_query &= paper_q
+            if filter_ev:
+                ev_filter &= paper_q.ev_filter()
+
+        # Unpack mesh ids.
+        mesh_ids = self._pop('mesh_ids', [])
+        if mesh_ids:
+            mesh_q = FromMeshIds(mesh_ids)
+            db_query &= mesh_q
+            if filter_ev:
+                ev_filter &= mesh_q.ev_filter()
+
+        # Check for health of the resulting query, and some other things.
+        if isinstance(db_query, EmptyQuery):
+            raise DbAPIError(f"No arguments from web query {self.web_query} "
+                             f"mapped to db query.")
+        assert isinstance(db_query, Query), "Somehow db_query is not Query."
+
+        component_queries = set(db_query.get_component_queries())
+        if require_any and not set(require_any) & component_queries:
+            raise DbAPIError(f'None of the required query elements '
+                             f'found: {require_any}')
+        if require_all and set(require_all) > component_queries:
+            raise DbAPIError(f"Required query elements not found: {require_all}")
+
+        if self.web_query and empty_web_query:
+            raise DbAPIError(f"Invalid query options: {self.web_query.keys()}.")
+
+        return db_query
+
 
 def iter_free_agents(query_dict):
     agent_keys = {k for k in query_dict.keys() if k.startswith('agent')}
@@ -341,106 +431,19 @@ def iter_free_agents(query_dict):
             yield entry
 
 
-def _db_query_from_web_query(query_dict, require_any=None, require_all=None,
-                             empty_web_query=False):
-    db_query = EmptyQuery()
-    num_agents = 0
-
-    logger.info(f"Making DB query from:\n{query_dict}")
-    filter_ev = query_dict.pop('filter_ev', 'false').lower() == 'true'
-    ev_filter = EvidenceFilter()
-
-    # Get the agents without specified locations (subject or object).
-    for raw_ag in iter_free_agents(query_dict):
-        ag, ns = process_agent(raw_ag)
-        db_query &= HasAgent(ag, namespace=ns)
-
-    # Get the agents with specified roles.
-    for role in ['subject', 'object']:
-        raw_ag = query_dict.pop(role, None)
-        if raw_ag is None:
-            continue
-        if isinstance(raw_ag, list):
-            assert len(raw_ag) == 1, f'Malformed agent for {role}: {raw_ag}'
-            raw_ag = raw_ag[0]
-        num_agents += 1
-        ag, ns = process_agent(raw_ag)
-        db_query &= HasAgent(ag, namespace=ns, role=role.upper())
-
-    # Get the raw name of the statement type (we allow for variation in case).
-    act_raw = query_dict.pop('type', None)
-    if act_raw is not None:
-        if isinstance(act_raw, list):
-            assert len(act_raw) == 1, \
-                f"Got multiple entries for statement type: {act_raw}."
-            act_raw = act_raw[0]
-        act = make_statement_camel(act_raw)
-        db_query &= HasType([act])
-
-    # Get whether the user wants a strict match
-    if _pop(query_dict, 'strict', False, bool):
-        db_query &= HasNumAgents((num_agents,))
-
-    # Unpack hashes, if present.
-    hashes = query_dict.pop('hashes', None)
-    if hashes:
-        db_query &= HasHash(hashes)
-
-    # Unpack paper ids, if present:
-    id_tpls = set()
-    for id_str in query_dict.pop('paper_ids', []):
-        val, typ = id_str.split('@')
-
-        # Turn tcids and trids into integers.
-        id_val = int(val) if typ in ['tcid', 'trid'] else val
-        id_tpls.add((typ, id_val))
-
-    if id_tpls:
-        paper_q = FromPapers(id_tpls)
-        db_query &= paper_q
-        if filter_ev:
-            ev_filter &= paper_q.ev_filter()
-
-    # Unpack mesh ids.
-    mesh_ids = query_dict.pop('mesh_ids', [])
-    if mesh_ids:
-        mesh_q = FromMeshIds(mesh_ids)
-        db_query &= mesh_q
-        if filter_ev:
-            ev_filter &= mesh_q.ev_filter()
-
-    # Check for health of the resulting query, and some other things.
-    if isinstance(db_query, EmptyQuery):
-        raise DbAPIError(f"No arguments from web query {query_dict} mapped to "
-                         f"db query.")
-    assert isinstance(db_query, Query), "Somehow db_query is not Query."
-
-    component_queries = set(db_query.get_component_queries())
-    if require_any and not set(require_any) & component_queries:
-        raise DbAPIError(f'None of the required query elements '
-                         f'found: {require_any}')
-    if require_all and set(require_all) > component_queries:
-        raise DbAPIError(f"Required query elements not found: {require_all}")
-
-    if query_dict and empty_web_query:
-        raise DbAPIError(f"Invalid query options: {query_dict.keys()}.")
-
-    return db_query
-
-
 class FromAgentsApiCall(StatementApiCall):
     def get_db_query(self):
         logger.info("Getting query details.")
         try:
-            inp_dict = {f'agent{i}': ag
-                        for i, ag in enumerate(self.web_query.poplist('agent'))}
-            inp_dict['mesh_ids'] = \
+            self.web_query.update(
+                {f'agent{i}': ag
+                 for i, ag in enumerate(self.web_query.poplist('agent'))}
+            )
+            self.web_query['mesh_ids'] = \
                 {m for m in self._pop('mesh_ids', '').split(',') if m}
-            inp_dict['paper_ids'] = \
+            self.web_query['paper_ids'] = \
                 {i for i in self._pop('paper_ids', '').split(',') if i}
-            inp_dict.update(self.web_query)
-            db_query = _db_query_from_web_query(
-                inp_dict,
+            db_query = self._db_query_from_web_query(
                 require_any={'HasAgent', 'FromPapers'},
                 empty_web_query=True
             )
@@ -464,15 +467,16 @@ class FromHashesApiCall(StatementApiCall):
                 Response("Too many hashes given, %d allowed." % MAX_STATEMENTS,
                          400))
 
-        return _db_query_from_web_query({'hashes': hashes})
+        self.web_query['hashes'] = hashes
+        return self._db_query_from_web_query()
 
 
 class FromHashApiCall(StatementApiCall):
     default_ev_lim = 1000
 
     def get_db_query(self):
-        hash = self._pop('hash')
-        return _db_query_from_web_query({'hashes': [hash]})
+        self.web_query['hashses'] = [self._pop('hash')]
+        return self._db_query_from_web_query()
 
 
 class FromPapersApiCall(StatementApiCall):
@@ -483,14 +487,9 @@ class FromPapersApiCall(StatementApiCall):
             logger.error("No ids provided!")
             abort(Response("No ids in request!", 400))
         mesh_ids = request.json.get('mesh_ids', [])
-        return _db_query_from_web_query({'paper_ids': ids,
-                                         'mesh_ids': mesh_ids})
-
-
-class QueryApiCall(ApiCall):
-    def get_db_query(self):
-        query_json = self._pop('json', '{}')
-        return Query.from_json(query_json)
+        self.web_query['papser_ids'] = ids
+        self.web_query['mesh_ids'] = mesh_ids
+        return self._db_query_from_web_query()
 
 
 class MetadataApiCall(FromAgentsApiCall):
@@ -522,6 +521,12 @@ class MetadataApiCall(FromAgentsApiCall):
         logger.info("Result prepared after %.2f seconds."
                     % sec_since(self.start_time))
         return resp
+
+
+class QueryApiCall(ApiCall):
+    def get_db_query(self):
+        query_json = self._pop('json', '{}')
+        return Query.from_json(query_json)
 
 
 # ==========================
@@ -698,17 +703,6 @@ def list_curations(stmt_hash, src_hash):
     curations = get_curations(pa_hash=stmt_hash, source_hash=src_hash)
     curation_json = [cur.to_json() for cur in curations]
     return jsonify(curation_json)
-
-
-def _pop(query, k, default=None, type_cast=None):
-    if isinstance(default, bool):
-        val = query.pop(k, str(default).lower()) == 'true'
-    else:
-        val = query.pop(k, default)
-
-    if type_cast is not None and val is not None:
-        return type_cast(val)
-    return val
 
 
 @dep_route('/metadata/<result_type>/from_agents', methods=['GET'])

8821ba734abd7cf1f0d723159f569739074fa06b
pagreene <patrick.anton.greene@gmail.com>
2020-07-06 12:07:47 -0400

    Don't dodge activeforms unnecessarily.

--- indra_db/client/readonly/interactions.py
+++ indra_db/client/readonly/interactions.py
@@ -6,8 +6,6 @@ from indra.statements import get_statement_by_name, Agent, ActiveForm
 def stmt_from_interaction(interaction):
     """Get a shell statement from an interaction."""
     StmtClass = get_statement_by_name(interaction['type'])
-    if issubclass(StmtClass, ActiveForm):
-        return None
     if interaction['type'] == 'Complex':
         agents = [Agent(name) for name in interaction['agents'].values()]
         stmt = StmtClass(agents)

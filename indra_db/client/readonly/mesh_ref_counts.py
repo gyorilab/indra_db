19b448e912598048a2ac50710c31bbbc2238375e
pagreene <patrick.anton.greene@gmail.com>
2020-09-14 08:59:55 -0400

    Update function to use new concept table.

--- indra_db/client/readonly/mesh_ref_counts.py
+++ indra_db/client/readonly/mesh_ref_counts.py
@@ -30,35 +30,49 @@ def get_mesh_ref_counts(mesh_terms, require_all=False, ro=None):
         ro = get_ro('primary')
 
     # Make sure the mesh IDs are of the correct kind.
-    if not all(m.startswith('D') for m in mesh_terms):
-        raise ValueError("All mesh terms must begin with D.")
+    if not all(m.startswith('D') or m.startswith('C') for m in mesh_terms):
+        raise ValueError("All mesh terms must begin with C or D.")
 
     # Convert the IDs to numbers for faster lookup.
-    mesh_num_map = {int(m[1:]): m for m in mesh_terms}
-
-    # Build the query.
-    t = ro.MeshRefCounts
-    nums = func.array_agg(t.mesh_num)
-    counts = func.array_agg(t.ref_count)
-    q = ro.session.query(t.mk_hash, nums.label('nums'),
-                         counts.label('ref_counts'), t.pmid_count)
-    if len(mesh_num_map.keys()) == 1:
-        q = q.filter(t.mesh_num == list(mesh_num_map.keys())[0])
-    elif len(mesh_num_map.keys()) > 1:
-        q = q.filter(t.mesh_num.in_(mesh_num_map.keys()))
-    else:
-        raise ValueError("Must submit at least one mesh term.")
-    q = q.group_by(t.mk_hash, t.pmid_count)
-
-    # Apply the require all option by comparing the length of the nums array
-    # to the number of inputs.
-    if require_all:
-        q = q.having(func.cardinality(nums) == len(mesh_num_map.keys()))
-
-    # Parse the results.
     result = {}
-    for mk_hash, nums, counts, pmid_count in q.all():
-        result[mk_hash] = {mesh_num_map[mesh_num]: ref_count
-                           for mesh_num, ref_count in zip(nums, counts)}
-        result[mk_hash]['total'] = pmid_count
+    for prefix, table in [('C', ro.MeshConceptRefCounts),
+                          ('D', ro.MeshTermRefCounts)]:
+        mesh_num_map = {int(m[1:]): m for m in mesh_terms
+                        if m.startswith(prefix)}
+        if not mesh_num_map:
+            continue
+
+        # Build the query.
+        nums = func.array_agg(table.mesh_num)
+        counts = func.array_agg(table.ref_count)
+        q = ro.session.query(table.mk_hash, nums.label('nums'),
+                             counts.label('ref_counts'), table.pmid_count)
+        if len(mesh_num_map.keys()) == 1:
+            q = q.filter(table.mesh_num == list(mesh_num_map.keys())[0])
+        elif len(mesh_num_map.keys()) > 1:
+            q = q.filter(table.mesh_num.in_(mesh_num_map.keys()))
+        q = q.group_by(table.mk_hash, table.pmid_count)
+
+        # Apply the require all option by comparing the length of the nums array
+        # to the number of inputs.
+        if require_all:
+            q = q.having(func.cardinality(nums) == len(mesh_num_map.keys()))
+
+        # Parse the results.
+        for mk_hash, nums, counts, pmid_count in q.all():
+            count_dict = {mesh_num_map[mesh_num]: ref_count
+                          for mesh_num, ref_count in zip(nums, counts)}
+            if mk_hash not in result:
+                result[mk_hash] = count_dict
+                result[mk_hash]['total'] = pmid_count
+            else:
+                result[mk_hash].update(count_dict)
+                result[mk_hash]['total'] += sum(counts)
+
+    # Little sloppy, but delete any that don't meet the require_all constraint.
+    if require_all:
+        num_terms = len(set(mesh_terms))
+        for mk_hash in result.copy().keys():
+            if len(result[mk_hash]) != num_terms + 1:
+                result.pop(mk_hash)
     return result

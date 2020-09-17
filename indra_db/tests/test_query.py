3950b320d88a6d0b0e8d14d7c34f26ba009a2223
pagreene <patrick.anton.greene@gmail.com>
2020-09-17 17:17:53 -0400

    Test for preservation of full/empty during inversion.

--- indra_db/tests/test_query.py
+++ indra_db/tests/test_query.py
@@ -466,6 +466,8 @@ def test_query_set_behavior():
                 assert False, "Inverted query is None."
             assert nq.is_inverse_of(q), "Inverse comparison failed! (nq vs. q)"
             assert q.is_inverse_of(nq), "Inverse comparison failed! (q vs. nq)"
+            assert q.empty == nq.full, "q.empty != nq.full."
+            assert q.full == nq.empty, "q.full != nq.empty."
             negative_result = dq(nq)
             assert negative_result == (all_hashes - result), \
                 'Negative result mismatch.'

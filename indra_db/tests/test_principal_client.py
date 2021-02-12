e7cb72273afc74b82ed915aafeb154f2f10ccc5b
Patrick Greene <patrick.anton.greene@gmail.com>
2021-02-12 14:15:02 -0500

    Add test for querying databases only.

--- indra_db/tests/test_principal_client.py
+++ indra_db/tests/test_principal_client.py
@@ -36,13 +36,18 @@ def _construct_database():
         [Complex([mek, erk]), Complex([raf, erk])]
     ])
 
+    db_builder.add_databases(['signor'])
+    db_builder.add_raw_database_statements([
+        [Complex([raf, erk])]
+    ])
+
     raw_stmts = db_builder.raw_statements
 
     pa_statements_pre = [
         (Phosphorylation(mek, erk), [raw_stmts[0], raw_stmts[2]]),
         (Complex([mek, erk]), [raw_stmts[1], raw_stmts[4]]),
         (Activation(mek, erk), [raw_stmts[3]]),
-        (Complex([raf, erk]), [raw_stmts[5]])
+        (Complex([raf, erk]), [raw_stmts[5], raw_stmts[6]])
     ]
     pa_stmts = []
     raw_unique_links = []
@@ -72,14 +77,14 @@ def _construct_database():
 def test_get_raw_statements_all():
     db = _construct_database()
     res = get_raw_stmt_jsons(db=db)
-    assert len(res) == 6
+    assert len(res) == 7, len(res)
 
 
 def test_raw_statement_retrieval_from_agents_type_only():
     db = _construct_database()
     res = get_raw_stmt_jsons_from_agents(stmt_type='Complex', db=db)
     assert len(res) > 0
-    assert len(res) < 6
+    assert len(res) < 7
     assert all(sj['type'] == 'Complex' for sj in res.values())
 
 
@@ -87,7 +92,7 @@ def test_raw_statement_retrieval_from_agents_mek():
     db = _construct_database()
     res = get_raw_stmt_jsons_from_agents(agents=[(None, 'MEK', 'FPLX')], db=db)
     assert len(res) > 0
-    assert len(res) < 6
+    assert len(res) < 7
     assert all('MEK' in json.dumps(sj) for sj in res.values())
 
 
@@ -97,8 +102,16 @@ def test_raw_statement_retrieval_generic():
                               db.Reading.id == db.RawStatements.reading_id],
                              db=db)
     assert len(res) > 0
-    assert len(res) < 6
+    assert len(res) < 7
     assert all(sj['evidence'][0]['source_api'] == 'reach'
+               for sj in res.values())
+
+
+def test_raw_statements_get_database_only():
+    db = _construct_database()
+    res = get_raw_stmt_jsons([db.RawStatements.reading_id.is_(None)], db=db)
+    assert len(res) == 1, len(res)
+    assert all(sj['evidence'][0]['source_api'] == 'signor'
                for sj in res.values())
 
 

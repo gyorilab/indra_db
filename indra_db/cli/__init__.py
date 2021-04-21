81ee8f57f36d84c0f74e117eadc1ead503890d5e
Patrick Greene <patrick.anton.greene@gmail.com>
2021-04-21 10:44:59 -0400

    Add sophisticated list feature to knowledgebase.

--- indra_db/cli/__init__.py
+++ indra_db/cli/__init__.py
@@ -14,20 +14,48 @@ def main():
 
 
 @main.command()
-@click.argument("mode", type=click.Choice(["upload", "update"]))
-def knowledgebase(mode):
+@click.argument("mode", type=click.Choice(["upload", "update", "list"]))
+@click.argument("sources", nargs=-1, type=click.STRING, required=False)
+def knowledgebase(mode, sources):
     """Upload or update the knowledge bases used by the database.
 
-    Use "upload" if the knowledge bases have not yet been added.
-    Use "update" if they have been added, but need to be updated.
+    Usage modes are:
+     - upload: use if the knowledge bases have not yet been added.
+     - update: if they have been added, but need to be updated.
+     - list: list the current knowledge sources that have been implemented.
+
+    Specify which knowledge base sources to update by their name, e.g. "Pathway
+    Commons" or "pc". If not specified, all sources will be updated.
     """
     from .knowledgebase import KnowledgebaseManager
+    if mode == 'list':
+        import tabulate
+        rows = []
+
+    source_set = None
+    if sources:
+        source_set = {s.lower() for s in sources}
 
     db = get_db('primary')
     for Manager in KnowledgebaseManager.__subclasses__():
         kbm = Manager()
-        print(kbm.name, '...')
+
+        # Skip entries that were not listed.
+        if source_set and kbm.name not in source_set \
+                and kbm.short_name not in source_set:
+            continue
+
+        # Perform the requested action.
         if mode == 'upload':
+            print(f'Uploading {kbm.name}...')
             kbm.upload(db)
         elif mode == 'update':
+            print(f'Updating {kbm.name}...')
             kbm.update(db)
+        elif mode == 'list':
+            rows.append([kbm.name, kbm.short_name])
+
+    if mode == 'list':
+        tbl = tabulate.tabulate(rows, headers=("Name", "Short Name"),
+                                tablefmt="simple")
+        print(tbl)

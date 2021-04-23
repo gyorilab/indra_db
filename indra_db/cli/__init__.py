a34837d1dfb3e61c410ab1bf18d350d0b58d55f1
Patrick Greene <patrick.anton.greene@gmail.com>
2021-04-23 10:05:03 -0400

    Add pipeline stats command.

--- indra_db/cli/__init__.py
+++ indra_db/cli/__init__.py
@@ -61,3 +61,25 @@ def knowledgebase(mode, sources):
                                 headers=("Name", "Short Name", "Last Updated"),
                                 tablefmt="simple")
         print(tbl)
+
+
+@main.command()
+@click.argument("task", type=click.Choice(["gather"]))
+def pipeline_stats(task):
+    """Manage the pipeline stats gathered on s3.
+
+    All major upload and update pipelines have basic timeing and success-failure
+    stats gather on them using the
+    :class:`DataGatherer <indra_db.util.data_gatherer.DataGatherer>` class
+    wrapper.
+
+    These stats are displayed on the ``/monitor`` endpoint of the database
+    service.
+
+    \b
+    Tasks are:
+     - gather: gather the individual job JSONs into an aggregated file.
+    """
+    if task == "gather":
+        from indra_db.util.data_gatherer import digest_s3_files
+        digest_s3_files()

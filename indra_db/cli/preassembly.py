698e09e727fc3b8edfdef4100b3e3f2f4173f08a
Patrick Greene <patrick.anton.greene@gmail.com>
2021-05-04 17:22:52 -0400

    Refactor preassembly tool to expose listing.
    This also refactors the method for getting a list of types
    to run by using a single query to get all latest raw statements,
    which proves to be many times faster than going type-by-type.

--- indra_db/cli/preassembly.py
+++ indra_db/cli/preassembly.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from datetime import datetime
 
 from indra_db import get_db
+from indra_db.exceptions import IndraDbException
 from indra_db.preassembly.submitter import VALID_STATEMENTS, \
     PreassemblySubmitter
 
@@ -10,7 +11,27 @@ def filter_updates(stmt_type, pa_updates):
     return {u.run_datetime for u in pa_updates if u.stmt_type == stmt_type}
 
 
-def run_preassembly(project_name):
+def list_last_updates(db):
+    """Return a dict of the most recent updates for each statement type."""
+    pa_updates = db.select_all(db.PreassemblyUpdates)
+    last_full_update = max(filter_updates(None, pa_updates))
+    last_updates = {st: max(filter_updates(st, pa_updates)
+                            | {last_full_update})
+                    for st in VALID_STATEMENTS}
+    return last_updates
+
+
+def list_latest_raw_stmts(db):
+    """Return a dict of the most recent new raw statement for each type."""
+    from sqlalchemy import func
+    res = (db.session.query(db.RawStatements.type,
+                            func.max(db.RawStatements.create_date))
+                     .group_by(db.RawStatements.type)
+                     .all())
+    return {k: v for k, v in res}
+
+
+def run_preassembly(mode, project_name):
     """Construct a submitter and begin submitting jobs to Batch for preassembly.
 
     This function will determine which statement types need to be updated and
@@ -27,21 +48,31 @@ def run_preassembly(project_name):
         purposes.
     """
     db = get_db('primary')
-    pa_updates = db.select_all(db.PreassemblyUpdates)
-    last_full_update = max(filter_updates(None, pa_updates))
-    last_updates = {st: max(filter_updates(st, pa_updates) | {last_full_update})
-                    for st in VALID_STATEMENTS}
+    if mode == 'update':
+        # Find the latest update for each statement type.
+        last_updates = list_last_updates(db)
 
-    need_to_update = []
-    for stmt_type, last_update in last_updates.items():
-        res = db.select_one(db.RawStatements,
-                            db.RawStatements.type == stmt_type,
-                            db.RawStatements.create_date > last_update)
-        if res:
-            need_to_update.append(stmt_type)
+        # Get the most recent raw statement datetimes
+        latest_raw_stmts = list_latest_raw_stmts(db)
 
+        # Only include statements types that have new raw statements.
+        need_to_update = [s_type for s_type, last_upd in last_updates.items()
+                          if s_type in latest_raw_stmts.keys()
+                          and latest_raw_stmts[s_type] > last_upd]
+    else:
+        # Make sure the pa_statements table is truly empty.
+        if db.select_one(db.PAStatements):
+            raise IndraDbException("Please clear the pa_statements table "
+                                   "before running create. If you want to run "
+                                   "an incremental update, please run with "
+                                   "mode 'update'.")
+
+        # Just run them all.
+        need_to_update = VALID_STATEMENTS[:]
+
+    # Create the submitter, and run it.
     basename = datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-    ps = PreassemblySubmitter(basename, 'update', project_name=project_name)
+    ps = PreassemblySubmitter(basename, mode, project_name=project_name)
     ps.set_max_jobs(4)
     ps.run(need_to_update, 100000, True, stagger=600, poll_interval=120)
 

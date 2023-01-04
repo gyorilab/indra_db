b483cc3d343df4b7222e00fdd9ac6b231bab57f6
kkaris <karis.klas@gmail.com>
2023-01-03 16:21:14 -0800

    Add TEMP_DIR to __all__

--- indra_db/readonly_dumping/locations.py
+++ indra_db/readonly_dumping/locations.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 __all__ = ["raw_statements_fpath", "source_counts_fpath", "text_refs_fpath",
            "reading_text_content_fpath", "drop_readings_fpath",
-           "reading_to_text_ref_map_fpath", "processed_stmts_fpath"]
+           "reading_to_text_ref_map_fpath", "processed_stmts_fpath",
+           "TEMP_DIR"]
 
 TEMP_DIR = os.environ.get("PRINCIPAL_DUMPING_ROOT")
 if not TEMP_DIR:

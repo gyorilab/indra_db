16543fd0cb51e16b3827ef6f1de8f4318734619f
kkaris <karis.klas@gmail.com>
2023-01-04 13:44:44 -0800

    Add option to remove evidence when loading statement json

--- indra_db/readonly_dumping/export_assembly.py
+++ indra_db/readonly_dumping/export_assembly.py
@@ -149,15 +149,23 @@ class StatementJSONDecodeError(Exception):
     pass
 
 
-def load_statement_json(json_str: str, attempt: int = 1, max_attempts: int = 5):
+def load_statement_json(
+        json_str: str,
+        attempt: int = 1,
+        max_attempts: int = 5,
+        remove_evidence: bool = False
+):
     try:
         return json.loads(json_str)
     except json.JSONDecodeError:
         if attempt < max_attempts:
             json_str = codecs.escape_decode(json_str)[0].decode()
-            return load_statement_json(
+            sj = load_statement_json(
                 json_str, attempt=attempt + 1, max_attempts=max_attempts
             )
+            if remove_evidence:
+                sj["evidence"] = []
+            return sj
     raise StatementJSONDecodeError(
         f"Could not decode statement JSON after " f"{attempt} attempts: {json_str}"
     )

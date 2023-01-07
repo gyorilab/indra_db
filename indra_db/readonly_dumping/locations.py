a2e472ce2d4749bf809a291e898d75e06e55a438
kkaris <karis.klas@gmail.com>
2023-01-06 20:12:22 -0800

    Add more files to locations

--- indra_db/readonly_dumping/locations.py
+++ indra_db/readonly_dumping/locations.py
@@ -5,7 +5,8 @@ __all__ = ["raw_statements_fpath", "source_counts_fpath", "text_refs_fpath",
            "reading_text_content_fpath", "drop_readings_fpath",
            "reading_to_text_ref_map_fpath", "processed_stmts_fpath",
            "grounded_stmts_fpath", "unique_stmts_fpath",
-           "refinements_fpath", "belief_scores_pkl_fpath", "TEMP_DIR"]
+           "refinements_fpath", "belief_scores_pkl_fpath", "TEMP_DIR",
+           "belief_scores_tsv_fpath", "reading_ref_link_tsv_fpath"]
 
 TEMP_DIR = os.environ.get("PRINCIPAL_DUMPING_ROOT")
 if not TEMP_DIR:
@@ -15,6 +16,7 @@ else:
 
 TEMP_DIR.mkdir(exist_ok=True, parents=True)
 
+# Dump files and their derivatives
 raw_statements_fpath = TEMP_DIR.joinpath("raw_statements.tsv.gz")
 reading_text_content_fpath = TEMP_DIR.joinpath("reading_text_content_meta.tsv.gz")
 text_refs_fpath = TEMP_DIR.joinpath("text_refs_principal.tsv.gz")
@@ -26,3 +28,7 @@ grounded_stmts_fpath = TEMP_DIR.joinpath("grounded_statements.tsv.gz")
 unique_stmts_fpath = TEMP_DIR.joinpath("unique_statements.tsv.gz")
 refinements_fpath = TEMP_DIR.joinpath("refinements.tsv.gz")
 belief_scores_pkl_fpath = TEMP_DIR.joinpath("belief_scores.pkl")
+
+# Temporary tsv files used for load into readonly db
+belief_scores_tsv_fpath = TEMP_DIR.joinpath("belief_scores.tsv")
+reading_ref_link_tsv_fpath = TEMP_DIR.joinpath("reading_ref_link.tsv")

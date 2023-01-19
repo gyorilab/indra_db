a0b2e8ac2cab1d05de13143feeba52e1576aad73
kkaris <karis.klas@gmail.com>
2023-01-19 14:33:53 -0800

    Remove duplicated code

--- indra_db/readonly_dumping/export_assembly.py
+++ indra_db/readonly_dumping/export_assembly.py
@@ -152,32 +152,6 @@ def load_text_refs_by_trid(fname: str) -> Dict:
     return text_refs
 
 
-class StatementJSONDecodeError(Exception):
-    pass
-
-
-def load_statement_json(
-        json_str: str,
-        attempt: int = 1,
-        max_attempts: int = 5,
-        remove_evidence: bool = False
-):
-    try:
-        return json.loads(json_str)
-    except json.JSONDecodeError:
-        if attempt < max_attempts:
-            json_str = codecs.escape_decode(json_str)[0].decode()
-            sj = load_statement_json(
-                json_str, attempt=attempt + 1, max_attempts=max_attempts
-            )
-            if remove_evidence:
-                sj["evidence"] = []
-            return sj
-    raise StatementJSONDecodeError(
-        f"Could not decode statement JSON after " f"{attempt} attempts: {json_str}"
-    )
-
-
 def reader_prioritize(reader_contents):
     drop = set()
     # We first organize the contents by source/text type

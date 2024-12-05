c3abf22ce71e8ed31cad1c0511a6d8bf7f4ff735
haohangyan <hy2664@nyu.edu>
2024-12-05 15:24:13 -0500

    Cleaned pipeline bash

--- indra_db/readonly_dumping/readonly_dumping_bash.sh
+++ indra_db/readonly_dumping/readonly_dumping_bash.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC1090
 # SETUP
 set -e
 # Get password to the principal database from the user
@@ -14,7 +15,9 @@ then
     exit 1
 fi
 
-# Set the user for the local db
+
+
+#Set the user for the local db
 LOCAL_RO_USER="postgres"
 export LOCAL_RO_USER
 
@@ -24,14 +27,9 @@ read -s LOCAL_RO_PASSWORD
 export LOCAL_RO_PASSWORD
 
 # Set the name of the local db
-LOCAL_RO_DB_NAME="indradb_readonly_local"
+LOCAL_RO_DB_NAME="indradb_readonly_local_test"
 export LOCAL_RO_DB_NAME
 echo "Local db name: $LOCAL_RO_DB_NAME"
-
-# Upload a JSON file with the start date to S3
-# This is used to keep track of the start date of the dump
-# The file is uploaded to the indra-db/dumps/ directory
-# The file name is the current date and time
 
 # Get the current date and time
 START_DATE_TIME=`date '+%Y-%m-%d %H:%M:%S'`
@@ -40,7 +38,6 @@ echo "{\"datetime\": \"$START_DATE_TIME\", \"date_stamp\": \"$START_DATE\"}" > s
 S3_PATH="s3://bigmech/indra-db/dumps/$START_DATE"
 aws s3 cp start.json "$S3_PATH/start.json"
 echo "Start date marked as: $START_DATE"
-
 # INITIAL DUMPING
 
 # Get file paths for initial dump files
@@ -51,7 +48,7 @@ export READING_TEXT_CONTENT_META_FPATH
 TEXT_REFS_PRINCIPAL_FPATH=`python3 -m indra_db.readonly_dumping.locations text_refs`
 export TEXT_REFS_PRINCIPAL_FPATH
 
-# Exit if any of the file names are empty
+
 if [ -z "$RAW_STMTS_FPATH" ] || [ -z "$READING_TEXT_CONTENT_META_FPATH" ] || [ -z "$TEXT_REFS_PRINCIPAL_FPATH" ]
 then
     if [ -z "$RAW_STMTS_FPATH" ]
@@ -73,14 +70,13 @@ else
     echo "Text refs principal file path: $TEXT_REFS_PRINCIPAL_FPATH"
 fi
 
-# Run dumps
-# Only run the dumps if the files don't exist
+# Exit if any of the file names are empty
 if [ ! -f "$RAW_STMTS_FPATH" ]
 then
     echo "Dumping raw statements"
     start=$(date +%s)
     psql -d indradb_test \
-         -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com \
+         -h indradb-refresh.cvyak4iikv71.us-east-1.rds.amazonaws.com \
          -U tester \
          -w \
          -c "COPY (SELECT id, db_info_id, reading_id,
@@ -100,7 +96,7 @@ then
     echo "Dumping reading text content meta"
     start=$(date +%s)
     psql -d indradb_test \
-         -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com \
+         -h indradb-refresh.cvyak4iikv71.us-east-1.rds.amazonaws.com \
          -U tester \
          -w \
          -c "COPY (SELECT rd.id, rd.reader_version, tc.id, tc.text_ref_id,
@@ -121,7 +117,7 @@ then
     echo "Dumping text refs principal"
     start=$(date +%s)
     psql -d indradb_test \
-         -h indradb-refresh.cwcetxbvbgrf.us-east-1.rds.amazonaws.com \
+         -h indradb-refresh.cvyak4iikv71.us-east-1.rds.amazonaws.com \
          -U tester \
          -w \
          -c "COPY (SELECT id, pmid, pmcid, doi, pii, url, manuscript_id
@@ -134,26 +130,25 @@ then
 else
     echo "Text refs principal file already exists, skipping dump"
 fi
-
 # LOCAL DB CREATION AND DUMPING
 
-# Run export assembly script
-python3 -m indra_db.readonly_dumping.export_assembly # --refresh-kb
+python -m indra_db.readonly_dumping.export_assembly
+python -m indra_db.readonly_dumping.export_assembly_refinement
 
-## Create db;
-#PGPASSWORD=$LOCAL_RO_PASSWORD
-#export PGPASSWORD
-#psql -h localhost -U postgres -c "DROP DATABASE IF EXISTS $LOCAL_RO_DB_NAME"
-#psql -h localhost -U postgres -c "CREATE DATABASE $LOCAL_RO_DB_NAME"
-#
+# Create db;
+PGPASSWORD=$LOCAL_RO_PASSWORD
+export PGPASSWORD
+
+psql -h localhost -U postgres -c "DROP DATABASE IF EXISTS $LOCAL_RO_DB_NAME"
+psql -h localhost -U postgres -c "CREATE DATABASE $LOCAL_RO_DB_NAME"
 ## Run import script
-#python3 -m indra_db.readonly_dumping.readonly_dumping \
-#        --db-name $LOCAL_RO_DB_NAME \
-#        --user $LOCAL_RO_USER \
-#        --password "$LOCAL_RO_PASSWORD"
-#        # --force  # Use if you want to overwrite an existing db, if it exists
-#
-## Dump the db, once done importing
+python3 -m indra_db.readonly_dumping.readonly_dumping \
+        --db-name $LOCAL_RO_DB_NAME \
+        --user $LOCAL_RO_USER \
+        --password "$LOCAL_RO_PASSWORD"
+        # --force  # Use if you want to overwrite an existing db, if it exists
+
+# Dump the db, once done importing
 #pg_dump -h localhost \
 #        -U postgres \
 #        -w \
@@ -161,27 +156,27 @@ python3 -m indra_db.readonly_dumping.export_assembly # --refresh-kb
 #
 ## copy to s3
 #aws s3 cp "${LOCAL_RO_DB_NAME}.dump" "s3://bigmech/indra-db/dumps/"
-#
-## Remove dump file only after it has been copied to s3 successfully
-##rm "${LOCAL_RO_DB_NAME}.dump"
-#
-## Upload an end date file to S3
-## This is used to keep track of the end date of the dump
-## The file is uploaded to the indra-db/dumps/ directory
-## The file name is the current date and time
-#
-## Get the current date and time
+
+# Remove dump file only after it has been copied to s3 successfully
+#rm "${LOCAL_RO_DB_NAME}.dump"
+
+# Upload an end date file to S3
+# This is used to keep track of the end date of the dump
+# The file is uploaded to the indra-db/dumps/ directory
+# The file name is the current date and time
+
+# Get the current date and time
 #END_DATE_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 #END_DATE=`date '+%Y-%m-%d'`
 #echo "{\"datetime\": \"$END_DATE_TIME\", \"date_stamp\": \"$END_DATE\"}" > end.json
 #aws s3 cp end.json "$S3_PATH/end.json"
-#
-## At this point, if a new readonly instance is already created, we could run
-## the following command to update the instance (assuming the password is set
-## in PGPASSWORD, which will be read if -w is set):
-## pg_restore -h <readonly-instance>.us-east-1.rds.amazonaws.com \
-##            -U <user-name> \
-##            -f <dump-file> \
-##            -w \
-##            -d indradb_readonly \
-##            --no-owner
+
+# At this point, if a new readonly instance is already created, we could run
+# the following command to update the instance (assuming the password is set
+# in PGPASSWORD, which will be read if -w is set):
+# pg_restore -h <readonly-instance>.us-east-1.rds.amazonaws.com \
+#            -U <user-name> \
+#            -f <dump-file> \
+#            -w \
+#            -d indradb_readonly \
+#            --no-owner

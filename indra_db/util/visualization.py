54b8177b3c79516a66de7f10224a0c085fbf9868
haohangyan <hy2664@nyu.edu>
2026-02-10 13:48:18 -0500

    Remove clean up for cache files

--- indra_db/util/visualization.py
+++ indra_db/util/visualization.py
@@ -729,14 +729,3 @@ if __name__ == '__main__':
 
     generate_all_plots(args.output_dir, refresh=args.refresh)
 
-    #clean up
-    file_names = [
-        belief_scores_pkl_fpath,
-        unique_stmts_fpath,
-        source_counts_fpath,
-    ]
-    for f in file_names:
-        if os.path.exists(f.absolute().as_posix()):
-            os.remove(f.absolute().as_posix())
-        else:
-            print(f"{f.absolute().as_posix()} does not exist.")

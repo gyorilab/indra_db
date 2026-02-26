b1aa24d3fda2ebabec03e6995aad2f8fc50b941c
kkaris <karis.klas@gmail.com>
2026-02-26 11:38:14 -0800

    Fix JSON response area with new sphinx plugin

--- doc/conf.py
+++ doc/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx_mdinclude',
     'sphinx_click',
     'sphinxcontrib.openapi',
+    'sphinx_design',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
